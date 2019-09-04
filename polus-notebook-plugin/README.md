aa7757e3058cc82e0aba4f21abf1a84348c903f6
Konstantin Taletskiy <konstantin@taletskiy.com>
2019-09-04 17:21:12 -0500

    feat: Initial release of Notebook executor plugin

---
+++ polus-notebook-plugin/README.md
@@ -0,0 +1,37 @@
+# Polus Jupyter Notebook Plugin
+
+This plugin enables rapid code prototyping in Polus. Users create the code in Polus Notebooks in variety of languages and then run the notebook as a WIPP plugin.
+Internally, Papermill is responsible for executing the notebook file.
+
+For more information on WIPP, visit the [official WIPP page](https://isg.nist.gov/deepzoomweb/software/wipp).
+
+**This plugin is in development and is subject for change**
+
+## Options
+
+This plugin takes two input parameters and one output parameter:
+
+| Name       | Description            | I/O    | Type |
+|------------|------------------------|--------|------|
+| `input`    | Input image collection | Input  | Path |
+| `notebook` | Notebook filename      | Input  | Path |
+| `output`   | Output image colee     | Output | List |
+
+
+## Build the plugin
+
+```bash
+docker build . -t labshare/polus-notebook-plugin:0.1.0
+```
+
+
+## Run the plugin
+
+### Manually
+
+```bash
+docker run -v /path/to/data:/data labshare/wipp-plugin-jupyter-notebook:0.0.0 \
+  --input /data/input \
+  --notebook /data/notebook.ipynb \
+  --output /data/output
+```
---
+++ polus-notebook-plugin/src/execute_notebook.py
@@ -0,0 +1,39 @@
+import argparse
+import os
+import time
+import pathlib
+import papermill as pm
+
+def main():
+    # Setup the Argument parsing
+    parser = argparse.ArgumentParser(prog='script', description='Script to execute Jupyter Notebooks')
+
+    # Parse input arguments from WIPP format: '--PARAMETER VALUE'
+    parser.add_argument('--input', dest='input', type=str, help='input image collection', required=True)
+    parser.add_argument('--notebook', dest='notebook', type=str, help='Jupyter notebook to run', required=True)
+    parser.add_argument('--output', dest='output', type=str, help='output directory', required=True)
+    args = parser.parse_args()
+    
+    input = '/data/inputs/notebooks/' + args.input
+    notebook = args.notebook
+    output = args.output
+
+    print('Arguments:')    
+    print(input)
+    print(notebook)
+    print(output)
+    
+    print('Beginning notebook execution...')
+    process_start = time.time()
+
+    pm.execute_notebook(
+       notebook,
+       '/tmp/output.ipynb',
+       parameters=dict(input_path=input, output_path=output)
+    )
+    
+    print(' ')
+    print('Execution completed in {} seconds!'.format(time.time() - process_start))
+        
+if __name__ == "__main__":
+    main()
