207ed64d1ba18a3f3d9de628de3485f73c71d222
Nick Schaub <nick.schaub@axleinfo.com>
2019-10-04 15:36:22 -0400

    Functional plugin, only processes first image

--- polus-precompute-slide-plugin/src/main.py
+++ polus-precompute-slide-plugin/src/main.py
@@ -2,10 +2,8 @@ from bfio.bfio import BioReader
 import numpy as np
 import json
 import copy
-import gzip
 import os
 from pathlib import Path
-import sys
 
 # Data types used by Neuroglancer
 NEUROGLANCER_DATA_TYPES = ("uint8", "uint16", "uint32", "uint64", "float32")
@@ -21,10 +19,89 @@ UNITS = {'m':  10**9,
 # Chunk Scale
 CHUNK_SIZE = 1024
 
+def _avg2(image):
+    image = image.astype('uint64')
+    y_max = image.shape[0] - image.shape[0] % 2
+    x_max = image.shape[1] - image.shape[1] % 2
+    avg_img = np.zeros(np.ceil([d/2 for d in image.shape]).astype('int'))
+    avg_img[0:int(y_max/2),0:int(x_max/2)]= (image[0:y_max-1:2,0:x_max-1:2] + \
+                                             image[1:y_max:2,0:x_max-1:2] + \
+                                             image[0:y_max-1:2,1:x_max:2] + \
+                                             image[1:y_max:2,1:x_max:2]) / 4
+    if y_max != image.shape[0]:
+        avg_img[-1,:int(x_max/2)] = (image[-1,0:x_max-1:2] + \
+                                     image[-1,1:x_max:2]) / 2
+    if x_max != image.shape[1]:
+        avg_img[:int(y_max/2),-1] = (image[0:y_max-1:2,-1] + \
+                                     image[1:y_max:2,-1]) / 2
+    if y_max != image.shape[0] and x_max != image.shape[1]:
+        avg_img[-1,-1] = image[-1,-1]
+
+    return avg_img
+
+def _get_lower_res(S,bfio_reader,slide_writer,encoder,logger,X=None,Y=None):
+    # Get the scale info
+    scale_info = None
+    for res in encoder.info['scales']:
+        if int(res['key'])==S:
+            scale_info = res
+            break
+    if scale_info==None:
+        ValueError("No scale information for resolution {}.".format(S))
+        
+    if X == None:
+        X = [0,scale_info['size'][0]]
+    if Y == None:
+        Y = [0,scale_info['size'][1]]
+    
+    # Modify upper bound to stay within resolution dimensions
+    if X[1] > scale_info['size'][0]:
+        X[1] = scale_info['size'][0]
+    if Y[1] > scale_info['size'][1]:
+        Y[1] = scale_info['size'][1]
+    
+    # Initialize the output
+    image = np.zeros((Y[1]-Y[0],X[1]-X[0]),dtype=bfio_reader.read_metadata().image().Pixels.get_PixelType())
+    
+    # If requesting from the lowest scale, then just read the image
+    if S==0:
+        image = bfio_reader.read_image(X=X,Y=Y).squeeze()
+    else:
+        # Set the subgrid dimensions
+        subgrid_dims = [[2*X[0],2*X[1]],[2*Y[0],2*Y[1]]]
+        for dim in subgrid_dims:
+            while dim[1]-dim[0] > CHUNK_SIZE:
+                dim.insert(1,dim[0] + ((dim[1] - dim[0]-1)//CHUNK_SIZE) * CHUNK_SIZE)
+        
+        for y in range(0,len(subgrid_dims[1])-1):
+            y_ind = [subgrid_dims[1][y] - subgrid_dims[1][0],subgrid_dims[1][y+1] - subgrid_dims[1][0]]
+            y_ind = [np.ceil(yi/2).astype('int') for yi in y_ind]
+            for x in range(0,len(subgrid_dims[0])-1):
+                x_ind = [subgrid_dims[0][x] - subgrid_dims[0][0],subgrid_dims[0][x+1] - subgrid_dims[0][0]]
+                x_ind = [np.ceil(xi/2).astype('int') for xi in x_ind]
+                sub_image = _get_lower_res(X=subgrid_dims[0][x:x+2],
+                                           Y=subgrid_dims[1][y:y+2],
+                                           S=S-1,
+                                           bfio_reader=bfio_reader,
+                                           slide_writer=slide_writer,
+                                           encoder=encoder,
+                                           logger=logger)
+                image[y_ind[0]:y_ind[1],x_ind[0]:x_ind[1]] = _avg2(sub_image)
+
+    # Rearrange the image for Neuroglancerx
+    image_shifted = np.moveaxis(image.reshape(image.shape[0],image.shape[1],1,1),
+                                (0, 1, 2, 3), (2, 3, 1, 0))
+    # Encode the chunk
+    image_encoded = encoder.encode(image_shifted)
+    # Write the chunk
+    slide_writer.store_chunk(image_encoded,str(S),(X[0],X[1],Y[0],Y[1],0,1))
+    
+    return image
+
 # Modified and condensed from FileAccessor class in neuroglancer-scripts
-# https://github.com/HumanBrainProject/neuroglancer-scripts/blob/master/src/neuroglancer_scripts/file_accessor.py
+# https://github.com/HumanBrainProject/neuroglancer-scripts/blob/master/src/neuroglancer_scripts/file_accessor.pyx_max != image.shape[1]
 class SlideWriter():
-    """Access a Neuroglancer pre-computed pyramid on the local file system.
+    """Write a Neuroglancer pre-computed pyramid on the local file system.
     :param str base_dir: path to the directory containing the pyramid
     """
 
@@ -61,7 +138,7 @@ class SlideWriter():
 # Modified and condensed from multiple functions and classes
 # https://github.com/HumanBrainProject/neuroglancer-scripts/blob/master/src/neuroglancer_scripts/chunk_encoding.py
 class ChunkEncoder:
-    """Encode/decode chunks from NumPy arrays to byte buffers.
+    """Encode/decode chunks from NumPy aout_pathrrays to byte buffers.
     :param str data_type: data type supported by Neuroglancer
     :param int num_channels: number of image channels
     """
@@ -88,6 +165,7 @@ class ChunkEncoder:
             raise KeyError("Invalid data_type {0} (should be one of {1})"
                                 .format(data_type, NEUROGLANCER_DATA_TYPES))
         
+        self.info = info
         self.num_channels = num_channels
         self.dtype = np.dtype(data_type).newbyteorder("<")
 
@@ -117,7 +195,7 @@ def bfio_metadata_to_slide_info(bfio_reader,outPath):
     resolution.append((phys_y[0] * UNITS[phys_y[1]] + phys_x[0] * UNITS[phys_x[1]])/2) # Just used as a placeholder
     dtype = bfio_reader.read_metadata().image().Pixels.get_PixelType()
     
-    # create a scales template, use the full resolution
+    # create a scales template, use the full resolution8
     scales = {
         "chunk_sizes":[[CHUNK_SIZE,CHUNK_SIZE,1]],
         "encoding":"raw",
@@ -136,15 +214,14 @@ def bfio_metadata_to_slide_info(bfio_reader,outPath):
     }
     
     num_scales = int(np.floor(np.min(np.log2(info['scales'][-1]['size'][0:2]))-np.log2(CHUNK_SIZE)))
-    num_scales = max(0,num_scales)
     
-    # for i in range(1,num_scales):
-    #     previous_scale = info['scales'][-1]
-    #     current_scale = copy.deepcopy(previous_scale)
-    #     current_scale['key'] = str(i)
-    #     current_scale['size'] = [np.round(previous_scale['size'][0]/2),np.round(previous_scale['size'][1]/2),1]
-    #     current_scale['resolution'] = [2*previous_scale['resolution'][0],2*previous_scale['resolution'][1],previous_scale['resolution'][2]]
-    #     info['scales'].append(current_scale)
+    for i in range(1,num_scales+1):
+        previous_scale = info['scales'][-1]
+        current_scale = copy.deepcopy(previous_scale)
+        current_scale['key'] = str(i)
+        current_scale['size'] = [int(np.ceil(previous_scale['size'][0]/2)),int(np.ceil(previous_scale['size'][1]/2)),1]
+        current_scale['resolution'] = [2*previous_scale['resolution'][0],2*previous_scale['resolution'][1],previous_scale['resolution'][2]]
+        info['scales'].append(current_scale)
         
     with open(op,'w') as writer:
         writer.write(json.dumps(info))
@@ -195,39 +272,41 @@ def main():
     logger.info('Done!')
     
     # Create the output path and info file
-    out_path = output_dir
-    file_info = bfio_metadata_to_slide_info(bf,out_path)
+    file_info = bfio_metadata_to_slide_info(bf,output_dir)
     logger.info("info:")
     logger.info(pprint.pformat(file_info))
     
     # Create the classes needed to generate a precomputed slice
+    logger.info("Creating encoder...")
     encoder = ChunkEncoder(file_info)
-    file_writer = SlideWriter(out_path)
-    
-    # Open the image in tiles, encode, and save
-    for Y in range(0, bf.num_y(),CHUNK_SIZE):
-        if Y+CHUNK_SIZE > bf.num_y():
-            Y_max = bf.num_y()
+    logger.info("Creating file_writer...")
+    file_writer = SlideWriter(output_dir)
+
+    for y in range(0,file_info['scales'][-1]['size'][1],CHUNK_SIZE):
+        logger.info("y: {}".format(y))
+        if y+CHUNK_SIZE > file_info['scales'][-1]['size'][1]:
+            y_max = file_info['scales'][-1]['size'][1]
         else:
-            Y_max = Y+CHUNK_SIZE
-        logger.info("Processing Y range: {}-{}".format(Y,Y_max))
-        for X in range(0,bf.num_x(),CHUNK_SIZE):
-            if X+CHUNK_SIZE > bf.num_x():
-                X_max = bf.num_x()
+            y_max = y+CHUNK_SIZE
+        for x in range(0,file_info['scales'][-1]['size'][0],CHUNK_SIZE):
+            logger.info("x: {}".format(x))
+            if x+CHUNK_SIZE > file_info['scales'][-1]['size'][0]:
+                x_max = file_info['scales'][-1]['size'][0]
             else:
-                X_max = X+CHUNK_SIZE
-            logger.info("Processing X range: {}-{}".format(X,X_max))
-            # Read the chunk
-            image = bf.read_image(X=(X,X_max),Y=(Y,Y_max))
-            image = image.reshape(image.shape[:-1])
-            image = np.moveaxis(image, (0, 1, 2, 3), (2, 3, 1, 0))
-            
+                x_max = x+CHUNK_SIZE
+        
+            logger.info("Run: _get_lower_res")
+            image = _get_lower_res(int(file_info['scales'][-1]['key']),bf,file_writer,encoder,logger,X=[x,x_max],Y=[y,y_max])
+        
+            # Rearrange the image for Neuroglancer
+            image_shifted = np.moveaxis(image.reshape(image.shape[0],image.shape[1],1,1),
+                                        (0, 1, 2, 3), (2, 3, 1, 0))
             # Encode the chunk
-            image_encoded = encoder.encode(image)
-            
+            image_encoded = encoder.encode(image_shifted)
             # Write the chunk
-            file_writer.store_chunk(image_encoded,"0",(X,X_max,Y,Y_max,0,1))
-            
+            logger.info("Saving (S,x-X,y-Y): ({},{}-{},{}-{})".format(file_info['scales'][-1]['key'],x,x_max,y,y_max))
+            file_writer.store_chunk(image_encoded,file_info['scales'][-1]['key'],(x,x_max,y,y_max,0,1))
+    
     logger.info("Finished precomputing. Closing the javabridge and exiting...")
     jutil.kill_vm()
 
