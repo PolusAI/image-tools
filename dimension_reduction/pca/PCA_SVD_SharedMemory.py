5421c78c8bf2c8855b3dd014e95ab637f317e505
Konstantin Taletskiy <konstantin@taletskiy.com>
2020-01-13 11:29:40 -0600

    fix: parameter check for boolean in WIPP

--- dimension_reduction/pca/PCA_SVD_SharedMemory.py
+++ dimension_reduction/pca/PCA_SVD_SharedMemory.py
@@ -79,7 +79,7 @@ np.savetxt ('PCs.csv', v.t().cpu().numpy(), delimiter=",")
 projectedData = torch.matmul(u,torch.diag(s)).to(device)
 
 #Compute the Standard Deviation of the projected data along each PC axis
-if args.computeStdev == 'yes':
+if args.computeStdev == 'true':
     Stdev = torch.std(projectedData,0).to(device)
     SumStdev = torch.sum(Stdev).to(device)
     normalizedStdev= torch.mul(torch.div(Stdev,SumStdev).to(device),100).to(device)
@@ -88,7 +88,7 @@ if args.computeStdev == 'yes':
 del u,s,v
 
 #Apply Sign Flip for the projected data
-if args.applySignFlip == 'yes':
+if args.applySignFlip == 'true':
     absProjectedData = torch.abs(projectedData).to(device)
     temp = torch.eq(absProjectedData,torch.max(absProjectedData,-2,keepdim=True).values).type(torch.FloatTensor).to(device)
     signMatrix = torch.sign(torch.sum(projectedData*temp,-2,keepdim=True).to(device)).to(device)
