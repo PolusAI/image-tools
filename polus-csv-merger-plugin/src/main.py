9aab8c9a3bbcffa408d696507c5c999a39095ccf
Nick Schaub <nick.schaub@axleinfo.com>
2020-01-14 22:34:02 -0500

    Added two methods of column merging

--- polus-csv-merger-plugin/src/main.py
+++ polus-csv-merger-plugin/src/main.py
@@ -35,112 +35,136 @@ if __name__=="__main__":
     same_rows = args.sameRows == 'true'
     logger.info('sameRows = {}'.format(stripExtension))
     
-    # Start the javabridge with proper java logging
+    # Get input files
     inpDir_files = [str(f.absolute()) for f in Path(inpDir).iterdir() if f.name.endswith('.csv')]
-
-    # Get the column headers
-    logger.info("Getting all unique headers...")
-    headers = set([])
-    for in_file in inpDir_files:
-        with open(in_file,'r') as fr:
-            line = fr.readline()
-            
-            if dim=='columns' and 'file' not in line[0:-1].split(','):
-                ValueError('No file columns found in csv: {}'.format(in_file))
-            
-            headers.update(line[0:-1].split(','))
-    if 'file' in headers:
-        headers.remove('file')
-    headers = list(headers)
-    headers.sort()
-    headers.insert(0,'file')
-    logger.info("Unique headers: {}".format(headers))
-
-    # Generate the line template
-    line_template = ','.join([('{' + h + '}') for h in headers]) + '\n'
-    line_dict = {key:'NaN' for key in headers}
+    inpDir_files.sort() # be a little fancy and merge alphabetically
     
-    # Generate the path to the output file
-    outPath = str(Path(outDir).joinpath('merged.csv').absolute())
-    
-    # Merge data
-    if dim=='rows':
-        logger.info("Merging the data along rows...")
-        with open(outPath,'w') as out_file:
-            out_file.write(','.join(headers) + '\n')
-            for f in inpDir_files:
-                file_dict = copy.deepcopy(line_dict)
-                if stripExtension:
-                    file_dict['file'] = str(Path(f).name).replace('.csv','')
-                else:
-                    file_dict['file'] = str(Path(f).name)
-                logger.info("Merging file: {}".format(file_dict['file']))
+    ''' If sameRows is set to true, nothing fancy to do. Just do the work and get out '''
+    if dim=='columns' and same_rows:
+            
+        # Determine the number of output files, and a list of files to be merged in each file
+        out_files = {}
+        for f in inpDir_files:
+            with open(f,'r') as fr:
+                count = sum(1 for row in fr)
+            if count not in out_files.keys():
+                out_files[count] = [f]
+            else:
+                out_files[count].append(f)
+            
+        count = 1
+        for key in out_files.keys():
+            outPath = str(Path(outDir).joinpath('merged_{}.csv'.format(count)).absolute())
+            count += 1
+            
+            inp_files = [open(f) for f in out_files[key]]
+            
+            with open(outPath,'w') as fw:
+                for l in range(key):
+                    fw.write(','.join([f.readline().rstrip('\n') for f in inp_files]))
+                    fw.write('\n')
+                    
+    else:
+        # Get the column headers
+        logger.info("Getting all unique headers...")
+        headers = set([])
+        for in_file in inpDir_files:
+            with open(in_file,'r') as fr:
+                line = fr.readline()
+                
+                if dim=='columns' and 'file' not in line[0:-1].split(','):
+                    ValueError('No file columns found in csv: {}'.format(in_file))
+                
+                headers.update(line[0:-1].split(','))
+        if 'file' in headers:
+            headers.remove('file')
+        headers = list(headers)
+        headers.sort()
+        headers.insert(0,'file')
+        logger.info("Unique headers: {}".format(headers))
 
-                with open(f,'r') as in_file:
-                    file_map = in_file.readline().rstrip('\n')
-                    file_map = file_map.split(',')
-                    numel = len(file_map)
-                    file_map = {ind:key for ind,key in enumerate(file_map)}
-
-                    for line in in_file:
-                        for el,val in enumerate(line.rstrip('\n').split(',')):
-                            file_dict[file_map[el]] = val
-                        out_file.write(line_template.format(**file_dict))
-    elif dim=='columns' and not same_rows:
-        logger.info("Merging the data along columns...")
+        # Generate the line template
+        line_template = ','.join([('{' + h + '}') for h in headers]) + '\n'
+        line_dict = {key:'NaN' for key in headers}
+        
+        # Generate the path to the output file
         outPath = str(Path(outDir).joinpath('merged.csv').absolute())
         
-        # Load the first csv and generate a dictionary to hold all values
-        out_dict = {}
-        with open(inpDir_files[0],'r') as in_file:
-            file_dict = copy.deepcopy(line_dict)
+        # Merge data
+        if dim=='rows':
+            logger.info("Merging the data along rows...")
+            with open(outPath,'w') as out_file:
+                out_file.write(','.join(headers) + '\n')
+                for f in inpDir_files:
+                    file_dict = copy.deepcopy(line_dict)
+                    if stripExtension:
+                        file_dict['file'] = str(Path(f).name).replace('.csv','')
+                    else:
+                        file_dict['file'] = str(Path(f).name)
+                    logger.info("Merging file: {}".format(file_dict['file']))
+
+                    with open(f,'r') as in_file:
+                        file_map = in_file.readline().rstrip('\n')
+                        file_map = file_map.split(',')
+                        numel = len(file_map)
+                        file_map = {ind:key for ind,key in enumerate(file_map)}
+
+                        for line in in_file:
+                            for el,val in enumerate(line.rstrip('\n').split(',')):
+                                file_dict[file_map[el]] = val
+                            out_file.write(line_template.format(**file_dict))
+        elif dim=='columns' and not same_rows:
+            logger.info("Merging the data along columns...")
+            outPath = str(Path(outDir).joinpath('merged.csv').absolute())
             
-            file_map = in_file.readline().rstrip('\n')
-            file_map = file_map.split(',')
-            numel = len(file_map)
-            file_map = {ind:key for ind,key in zip(range(numel),file_map)}
-            
-            for line in in_file:
-                file_dict = copy.deepcopy(line_dict)
-                for el,val in enumerate(line.rstrip('\n').split(',')):
-                    file_dict[file_map[el]] = val
-                if file_dict['file'] in out_dict.keys():
-                    UserWarning('Skipping row for file since it is already in the output file dictionary: {}'.format(file_dict['file']))
-                else:
-                    out_dict[file_dict['file']] = file_dict
-                    
-        # Loop through the remaining files and update the output dictionary
-        for f in inpDir_files[1:]:
-            
-            with open(f,'r') as in_file:
+            # Load the first csv and generate a dictionary to hold all values
+            out_dict = {}
+            with open(inpDir_files[0],'r') as in_file:
                 file_dict = copy.deepcopy(line_dict)
                 
                 file_map = in_file.readline().rstrip('\n')
                 file_map = file_map.split(',')
-                file_ind = [i for i,v in enumerate(file_map) if v == 'file'][0]
                 numel = len(file_map)
                 file_map = {ind:key for ind,key in zip(range(numel),file_map)}
                 
                 for line in in_file:
                     file_dict = copy.deepcopy(line_dict)
-                    line_vals = line.rstrip('\n').split(',')
-                    for el,val in enumerate(line_vals):
-                        if line_vals[file_ind] not in out_dict.keys():
-                            out_dict[line_vals[file_ind]] = copy.deepcopy(line_dict)
-                            out_dict[line_vals[file_ind]]['file'] = line_vals[file_ind]
-                        if el == file_ind:
-                            continue
-                        if out_dict[line_vals[file_ind]][file_map[el]] != 'NaN':
-                            Warning('Skipping duplicate value ({}) found in {}'.format(val,in_file))
-                        else:
-                            out_dict[line_vals[file_ind]][file_map[el]] = val
-                            
-        # Write the output file
-        with open(outPath,'w') as out_file:
-            out_file.write(','.join(headers) + '\n')
-            
-            for val in out_dict.values():
-                out_file.write(line_template.format(**val))
-    
-    elif dim=='columns':
-        pass
+                    for el,val in enumerate(line.rstrip('\n').split(',')):
+                        file_dict[file_map[el]] = val
+                    if file_dict['file'] in out_dict.keys():
+                        UserWarning('Skipping row for file since it is already in the output file dictionary: {}'.format(file_dict['file']))
+                    else:
+                        out_dict[file_dict['file']] = file_dict
+                        
+            # Loop through the remaining files and update the output dictionary
+            for f in inpDir_files[1:]:
+                
+                with open(f,'r') as in_file:
+                    file_dict = copy.deepcopy(line_dict)
+                    
+                    file_map = in_file.readline().rstrip('\n')
+                    file_map = file_map.split(',')
+                    file_ind = [i for i,v in enumerate(file_map) if v == 'file'][0]
+                    numel = len(file_map)
+                    file_map = {ind:key for ind,key in zip(range(numel),file_map)}
+                    
+                    for line in in_file:
+                        file_dict = copy.deepcopy(line_dict)
+                        line_vals = line.rstrip('\n').split(',')
+                        for el,val in enumerate(line_vals):
+                            if line_vals[file_ind] not in out_dict.keys():
+                                out_dict[line_vals[file_ind]] = copy.deepcopy(line_dict)
+                                out_dict[line_vals[file_ind]]['file'] = line_vals[file_ind]
+                            if el == file_ind:
+                                continue
+                            if out_dict[line_vals[file_ind]][file_map[el]] != 'NaN':
+                                Warning('Skipping duplicate value ({}) found in {}'.format(val,in_file))
+                            else:
+                                out_dict[line_vals[file_ind]][file_map[el]] = val
+                                
+            # Write the output file
+            with open(outPath,'w') as out_file:
+                out_file.write(','.join(headers) + '\n')
+                
+                for val in out_dict.values():
+                    out_file.write(line_template.format(**val))
