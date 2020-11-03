236721027c723e1f5f2fb9ec0c17d5c736911185
Nick Schaub <nicholas.j.schaub@gmail.com>
2020-11-03 10:25:55 -0500

    Fixed documentation path

--- utils/polus-bfio-util-py/docs/source/conf.py
+++ utils/polus-bfio-util-py/docs/source/conf.py
@@ -13,7 +13,7 @@
 import os
 import sys
 from pathlib import Path
-sys.path.insert(0, os.path.abspath('../..'))
+sys.path.insert(0, str(Path(__file__).parent.parent.parent.absolute()))
 sys.setrecursionlimit(1500)
 
 
