4791c2133fa0441e065640e9ee9d9ea44d2e5d64
gauharbains <gauhar.bains@labshare.org>
2020-12-03 19:51:10 +0000

    improved javabridge handling

--- polus-aics-classic-seg-plugin/src/Workflows/Playground_filament3d.py
+++ polus-aics-classic-seg-plugin/src/Workflows/Playground_filament3d.py
@@ -2,9 +2,6 @@ import numpy as np
 import os
 import cv2
 import logging, sys
-import traceback
-import javabridge as jutil
-import bioformats
 from bfio import BioReader, BioWriter
 from pathlib import Path
 from aicsimageio import AICSImage
@@ -28,62 +25,47 @@ def segment_images(inpDir, outDir, config_data):
     logger = logging.getLogger("main")
     logger.setLevel(logging.INFO)
 
-    try : 
-        logger.info('Initializing the javabridge...')
-        log_config = Path(__file__).parent.parent.joinpath("log4j.properties")
-        jutil.start_vm(args=["-Dlog4j.configuration=file:{}".format(str(log_config.absolute()))],class_path=bioformats.JARS)
-
-        inpDir_files = os.listdir(inpDir)
-        for i,f in enumerate(inpDir_files):
-            logger.info('Segmenting image : {}'.format(f))
-            
-            # Load image
-            br = BioReader(os.path.join(inpDir,f))
-            image = br.read_image()
-            structure_channel = 0 
-            struct_img0 = image[:,:,:,structure_channel,0]
-            struct_img0 = struct_img0.transpose(2,0,1).astype(np.float32)
-
-            # main algorithm
-            intensity_scaling_param = config_data['intensity_scaling_param']
-            struct_img = intensity_normalization(struct_img0, scaling_param=intensity_scaling_param)
-            gaussian_smoothing_sigma = config_data['gaussian_smoothing_sigma']
-    
-            if config_data['preprocessing_function'] == 'image_smoothing_gaussian_3d':
-                structure_img_smooth = image_smoothing_gaussian_3d(struct_img, sigma=gaussian_smoothing_sigma)
-            elif config_data['preprocessing_function'] == 'edge_preserving_smoothing_3d':
-                structure_img_smooth = edge_preserving_smoothing_3d(struct_img)        
-
-            f3_param = config_data['f3_param']
-            bw = filament_3d_wrapper(structure_img_smooth, f3_param)
-            minArea = config_data['minArea']
-            seg = remove_small_objects(bw>0, min_size=minArea, connectivity=1, in_place=False)
-            seg = seg >0
-            out_img=seg.astype(np.uint8)
-            out_img[out_img>0]=255  
-
-            # create output image
-            out_img = out_img.transpose(1,2,0)
-            out_img = out_img.reshape((out_img.shape[0], out_img.shape[1], out_img.shape[2], 1, 1))
-
-            # write image using BFIO
-            bw = BioWriter(os.path.join(outDir,f))
-            bw.num_x(out_img.shape[1])
-            bw.num_y(out_img.shape[0])
-            bw.num_z(out_img.shape[2])
-            bw.num_c(out_img.shape[3])
-            bw.num_t(out_img.shape[4])
-            bw.pixel_type(dtype='uint8')
-            bw.write_image(out_img)
-            bw.close_image()
-
-    except Exception:
-        traceback.print_exc()
-
-    finally:
-        # Close the javabridge regardless of successful completion
-        logger.info('Closing the javabridge')
-        jutil.kill_vm()
+    inpDir_files = os.listdir(inpDir)
+    for i,f in enumerate(inpDir_files):
+        logger.info('Segmenting image : {}'.format(f))
         
-        # Exit the program
-        sys.exit()
+        # Load image
+        br = BioReader(os.path.join(inpDir,f))
+        image = br.read_image()
+        structure_channel = 0 
+        struct_img0 = image[:,:,:,structure_channel,0]
+        struct_img0 = struct_img0.transpose(2,0,1).astype(np.float32)
+
+        # main algorithm
+        intensity_scaling_param = config_data['intensity_scaling_param']
+        struct_img = intensity_normalization(struct_img0, scaling_param=intensity_scaling_param)
+        gaussian_smoothing_sigma = config_data['gaussian_smoothing_sigma']
+
+        if config_data['preprocessing_function'] == 'image_smoothing_gaussian_3d':
+            structure_img_smooth = image_smoothing_gaussian_3d(struct_img, sigma=gaussian_smoothing_sigma)
+        elif config_data['preprocessing_function'] == 'edge_preserving_smoothing_3d':
+            structure_img_smooth = edge_preserving_smoothing_3d(struct_img)        
+
+        f3_param = config_data['f3_param']
+        bw = filament_3d_wrapper(structure_img_smooth, f3_param)
+        minArea = config_data['minArea']
+        seg = remove_small_objects(bw>0, min_size=minArea, connectivity=1, in_place=False)
+        seg = seg >0
+        out_img=seg.astype(np.uint8)
+        out_img[out_img>0]=255  
+
+        # create output image
+        out_img = out_img.transpose(1,2,0)
+        out_img = out_img.reshape((out_img.shape[0], out_img.shape[1], out_img.shape[2], 1, 1))
+
+        # write image using BFIO
+        bw = BioWriter(os.path.join(outDir,f))
+        bw.num_x(out_img.shape[1])
+        bw.num_y(out_img.shape[0])
+        bw.num_z(out_img.shape[2])
+        bw.num_c(out_img.shape[3])
+        bw.num_t(out_img.shape[4])
+        bw.pixel_type(dtype='uint8')
+        bw.write_image(out_img)
+        bw.close_image()
+
--- polus-aics-classic-seg-plugin/src/Workflows/Playground_gja1.py
+++ polus-aics-classic-seg-plugin/src/Workflows/Playground_gja1.py
@@ -2,9 +2,6 @@ import numpy as np
 import os
 import cv2
 import logging, sys
-import traceback
-import javabridge as jutil
-import bioformats
 from bfio import BioReader, BioWriter
 from pathlib import Path
 from aicsimageio import AICSImage
@@ -27,58 +24,44 @@ def segment_images(inpDir, outDir, config_data):
                         datefmt='%d-%b-%y %H:%M:%S')
     logger = logging.getLogger("main")
     logger.setLevel(logging.INFO)
-
-    try : 
-        logger.info('Initializing the javabridge...')
-        log_config = Path(__file__).parent.parent.joinpath("log4j.properties")
-        jutil.start_vm(args=["-Dlog4j.configuration=file:{}".format(str(log_config.absolute()))],class_path=bioformats.JARS)
+ 
+    inpDir_files = os.listdir(inpDir)
+    for i,f in enumerate(inpDir_files):
+        logger.info('Segmenting image : {}'.format(f))
         
-        inpDir_files = os.listdir(inpDir)
-        for i,f in enumerate(inpDir_files):
-            logger.info('Segmenting image : {}'.format(f))
-            
-            # Load image
-            br = BioReader(os.path.join(inpDir,f))
-            image = br.read_image()
-            structure_channel = 0 
-            struct_img0 = image[:,:,:,structure_channel,0]
-            struct_img0 = struct_img0.transpose(2,0,1).astype(np.float32)
+        # Load image
+        br = BioReader(os.path.join(inpDir,f))
+        image = br.read_image()
+        structure_channel = 0 
+        struct_img0 = image[:,:,:,structure_channel,0]
+        struct_img0 = struct_img0.transpose(2,0,1).astype(np.float32)
 
-            # main algorithm
-            intensity_scaling_param = config_data['intensity_scaling_param']
-            struct_img = intensity_normalization(struct_img0, scaling_param=intensity_scaling_param) 
-            gaussian_smoothing_sigma = config_data['gaussian_smoothing_sigma'] 
-            structure_img_smooth = image_smoothing_gaussian_slice_by_slice(struct_img, sigma=gaussian_smoothing_sigma)
-            s3_param = config_data['s3_param']
-            bw = dot_3d_wrapper(structure_img_smooth, s3_param)
-            minArea = config_data['minArea']
-            seg = remove_small_objects(bw>0, min_size=minArea, connectivity=1, in_place=False)
-            seg = seg >0
-            out_img=seg.astype(np.uint8)
-            out_img[out_img>0]=255
+        # main algorithm
+        intensity_scaling_param = config_data['intensity_scaling_param']
+        struct_img = intensity_normalization(struct_img0, scaling_param=intensity_scaling_param) 
+        gaussian_smoothing_sigma = config_data['gaussian_smoothing_sigma'] 
+        structure_img_smooth = image_smoothing_gaussian_slice_by_slice(struct_img, sigma=gaussian_smoothing_sigma)
+        s3_param = config_data['s3_param']
+        bw = dot_3d_wrapper(structure_img_smooth, s3_param)
+        minArea = config_data['minArea']
+        seg = remove_small_objects(bw>0, min_size=minArea, connectivity=1, in_place=False)
+        seg = seg >0
+        out_img=seg.astype(np.uint8)
+        out_img[out_img>0]=255
 
-            # create output image
-            out_img = out_img.transpose(1,2,0)
-            out_img = out_img.reshape((out_img.shape[0], out_img.shape[1], out_img.shape[2], 1, 1))
+        # create output image
+        out_img = out_img.transpose(1,2,0)
+        out_img = out_img.reshape((out_img.shape[0], out_img.shape[1], out_img.shape[2], 1, 1))
 
-            # write image using BFIO
-            bw = BioWriter(os.path.join(outDir,f))
-            bw.num_x(out_img.shape[1])
-            bw.num_y(out_img.shape[0])
-            bw.num_z(out_img.shape[2])
-            bw.num_c(out_img.shape[3])
-            bw.num_t(out_img.shape[4])
-            bw.pixel_type(dtype='uint8')
-            bw.write_image(out_img)
-            bw.close_image()
+        # write image using BFIO
+        bw = BioWriter(os.path.join(outDir,f))
+        bw.num_x(out_img.shape[1])
+        bw.num_y(out_img.shape[0])
+        bw.num_z(out_img.shape[2])
+        bw.num_c(out_img.shape[3])
+        bw.num_t(out_img.shape[4])
+        bw.pixel_type(dtype='uint8')
+        bw.write_image(out_img)
+        bw.close_image()
 
-    except Exception:
-        traceback.print_exc()
 
-    finally:
-        # Close the javabridge regardless of successful completion
-        logger.info('Closing the javabridge')
-        jutil.kill_vm()
-        
-        # Exit the program
-        sys.exit()
