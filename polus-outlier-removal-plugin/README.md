163ad0130866d949e478e936b28d1df7aa182ede
Nagarajan <nagarajanj2@nih.gov>
2021-01-06 09:46:37 -0500

    Version 0.1.0

---
+++ polus-outlier-removal-plugin/README.md
@@ -0,0 +1,35 @@
+﻿# Outlier removal
+The outlier removal plugin removes the outliers from the data using Isolation Forest algorithm and outputs csv file. The output is separate csv files for inliers and outliers. The input file should be in csv format.
+
+## Inputs:
+### Input csv collection:
+The input file that need outliers to be removed. The file should be in csv format. This is a required parameter for the plugin.
+
+## Methods:
+Choose any one of the method mentioned to remove outliers from the data.
+
+### Isolation Forest
+Ensemble-based unsupervised method for outlier detection. The algorithm isolates outliers instead of normal instances. It works based on the principle that outliers are few and different and hence, the outliers can be identified easier than the normal points. The score is calculated as the path length to isolate the observation.
+
+## Output:
+The output is separate csv files for inliers and outliers.
+
+## Building
+
+To build the Docker image for the conversion plugin, run
+`./build-docker.sh`.
+
+## Install WIPP Plugin
+
+If WIPP is running, navigate to the plugins page and add a new plugin. Paste the contents of `plugin.json` into the pop-up window and submit.
+For more information on WIPP, visit the [official WIPP page](https://isg.nist.gov/deepzoomweb/software/wipp).
+
+## Options
+
+This plugin takes four input argument if methods other than 'Manual' is selected else three input arguments and one output argument:
+
+| Name                   | Description             | I/O    | Type   |
+|------------------------|-------------------------|--------|--------|
+| `--inpdir` | Input csv collection| Input | csvCollection |
+| `--methods` | Select methods for outlier removal | Input | enum|
+| `--outdir` | Output collection | Output | csvCollection |
---
+++ polus-outlier-removal-plugin/src/main.py
@@ -0,0 +1,112 @@
+import argparse
+import logging
+import os
+import fnmatch
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import IsolationForest
+from sklearn.metrics import precision_score, recall_score, f1_score, roc_auc_score, accuracy_score, confusion_matrix,classification_report
+
+# Initialize the logger
+logging.basicConfig(format='%(asctime)s - %(name)-8s - %(levelname)-8s - %(message)s',
+                    datefmt='%d-%b-%y %H:%M:%S')
+logger = logging.getLogger("main")
+logger.setLevel(logging.INFO)
+
+def list_file(csv_directory):
+    """List all the .csv files in the directory.
+    
+    Args:
+        csv_directory (str): Path to the directory containing the csv files.
+        
+    Returns:
+        The path to directory, list of names of the subdirectories in dirpath (if any) and the filenames of .csv files.
+        
+    """
+    list_of_files = [os.path.join(dirpath, file_name)
+                     for dirpath, dirnames, files in os.walk(csv_directory)
+                     for file_name in fnmatch.filter(files, '*.csv')]
+    return list_of_files
+
+def isolationforest(data_set):
+    """Detects outliers using Isolation Forest algorithm.
+    
+    Args:
+        data_set (array): Input data.
+        
+    Returns:
+        ndarray with (+1 or -1) whether or not it should be considered as an inlier according to the fitted model.
+    
+    """
+    clf = IsolationForest(random_state=9,n_estimators=100)
+    clf.fit(data_set)
+    pred = clf.predict(data_set)
+    return pred
+ 
+# Setup the argument parsing
+def main():
+    logger.info("Parsing arguments...")
+    parser = argparse.ArgumentParser(prog='main', description='K-means clustering plugin.')
+    parser.add_argument('--inpdir', dest='inpdir', type=str,
+                        help='Input collection-Data need to be clustered', required=True)
+    parser.add_argument('--methods', dest='methods', type=str,
+                        help='Select Manual or Elbow or Calinski Harabasz or Davies Bouldin method', required=False)
+    parser.add_argument('--outdir', dest='outdir', type=str,
+                        help='Output collection', required=True)
+    
+    # Parse the arguments
+    args = parser.parse_args()  
+    
+    #Path to csvfile directory
+    inpdir = args.inpdir
+    logger.info('inpdir = {}'.format(inpdir))
+    
+    #Determine k-value using different methods
+    methods = args.methods
+    logger.info('methods = {}'.format(methods))
+    
+    #Path to save output csvfiles
+    outdir = args.outdir
+    logger.info('outdir = {}'.format(outdir))
+    
+    #Get list of .csv files in the directory including sub folders for clustering
+    inputcsv = list_file(inpdir)
+    if not inputcsv:
+        raise ValueError('No .csv files found.')
+            
+    #Dictionary of methods to determine k-value
+    FEAT = {'IsolationForest': isolationforest}
+       
+    for inpfile in inputcsv:
+        #Get the full path
+        split_file = os.path.normpath(inpfile)
+        
+        #split to get only the filename
+        inpfilename = os.path.split(split_file)
+        file_name_csv = inpfilename[-1]
+        file_name,file_name1 = file_name_csv.split('.', 1)
+        
+        #Read csv file
+        logger.info('Started reading the file ' + file_name)
+        df = pd.read_csv(inpfile)
+        
+        #Standardize the data
+        data = StandardScaler().fit_transform(df)
+        
+        #Detect outliers
+        rem_out = FEAT[methods](data)
+        df['anomaly']= rem_out
+        inliers = df.loc[df['anomaly']==1]
+        outliers = df.loc[df['anomaly']==-1]
+        inliers = inliers.drop('anomaly',axis=1)
+        outliers = outliers.drop('anomaly',axis=1)
+
+    	#Save dataframe into csv file
+        os.chdir(outdir)
+        logger.info('Saving csv file')
+        export_inlier = inliers.to_csv (r'%s_inliers.csv'%file_name, index=None, header=True, encoding='utf-8-sig')
+        export_outlier = outliers.to_csv (r'%s_outliers.csv'%file_name, index=None, header=True, encoding='utf-8-sig')
+        logger.info("Finished all processes!")
+
+if __name__ == "__main__":
+    main()
