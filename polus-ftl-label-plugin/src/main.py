bdbddc311a400f0459938a4ba641b36b1dc71494
Nicholas-Schaub <nick.schaub@axleinfo.com>
2021-02-12 17:09:57 -0500

    Fixed data input/output type bug

--- polus-ftl-label-plugin/src/main.py
+++ polus-ftl-label-plugin/src/main.py
@@ -4,18 +4,18 @@ from preadator import ProcessManager
 import numpy as np
 
 def label_thread(input_path,output_path,connectivity):
-    
+
     with ProcessManager.thread() as active_threads:
-        with bfio.BioReader(input_path,max_workers=active_threads.count) as br:
-            with bfio.BioWriter(output_path,max_workers=active_threads.count,metadata=br.metadata) as bw:
-            
+        with bfio.BioReader(input_path,max_workers=2) as br:
+            with bfio.BioWriter(output_path,max_workers=2,metadata=br.metadata) as bw:
                 # Load an image and convert to binary
                 image = br[...,0,0]>0
-                
+
                 # Run the labeling algorithm
                 labels = ftl.label_nd(image.squeeze(),connectivity)
-                
+
                 # Save the image
+                bw.dtype = labels.dtype
                 bw[:] = labels
 
 if __name__=="__main__":
@@ -34,7 +34,7 @@ if __name__=="__main__":
                         help='Input image collection to be processed by this plugin', required=True)
     parser.add_argument('--outDir', dest='outDir', type=str,
                         help='Output collection', required=True)
-    
+
     # Parse the arguments
     args = parser.parse_args()
     connectivity = int(args.connectivity)
@@ -43,17 +43,18 @@ if __name__=="__main__":
     logger.info('inpDir = {}'.format(inpDir))
     outDir = Path(args.outDir)
     logger.info('outDir = {}'.format(outDir))
-    
+
     # We only need a thread manager since labeling and image reading/writing
     # release the gil
     ProcessManager.init_threads()
-    
+
     # Get all file names in inpDir image collection
     files = [f for f in inpDir.iterdir() if f.is_file() and f.name.endswith('.ome.tif')]
-    
+
     for file in files:
-        
+
         ProcessManager.submit_thread(label_thread,
                                      file,outDir.joinpath(file.name),connectivity)
-        
+
     ProcessManager.join_threads()
+
