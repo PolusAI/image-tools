b78553123cc33a199ac15e456546bc7e55ac80c1
mmvih <mmvihani@gmail.com>
2021-03-16 17:13:45 +0000

    area filtering with min and max

--- polus-binary-operations-plugin/src/main.py
+++ polus-binary-operations-plugin/src/main.py
@@ -11,41 +11,32 @@ from scipy import ndimage
 
 Tile_Size = 256
 
-def invert_binary(image, kernel=None, intk=None, n=None):
+def invert_binary(image, kernel=None, n=None):
     invertedimg = np.zeros(image.shape,dtype=datatype)
-    invertedimg = 255 - image
+    invertedimg = 1 - image
     return invertedimg
 
-def dilate_binary(image,kernel=None, intk=None, n=None):
-    image[image == 255] = 1
-    dilatedimg = cv2.dilate(image, kernel, iterations=1)
-    dilatedimg[dilatedimg == 1] = 255
+def dilate_binary(image, kernel=None, n=None): 
+    dilatedimg = cv2.dilate(image, kernel, iterations=n)
     return dilatedimg
 
-def erode_binary(image,kernel=None, intk=None, n=None):
-    image[image == 255] = 1
-    erodedimg = cv2.erode(image, kernel, iterations=1)
-    erodedimg[erodedimg == 1] = 255
+def erode_binary(image,kernel=None, n=None):
+    erodedimg = cv2.erode(image, kernel, iterations=n)
     return erodedimg
 
-def open_binary(image,kernel=None, intk=None, n=None):
-    image[image == 255] = 1
+def open_binary(image,kernel=None, n=None):
     openimg = cv2.morphologyEx(image, cv2.MORPH_OPEN, kernel)
     return openimg
 
-def close_binary(image,kernel=None, intk=None, n=None):
-    image[image == 255] = 1
+def close_binary(image,kernel=None, n=None):
     closeimg = cv2.morphologyEx(image, cv2.MORPH_CLOSE, kernel)
-    closeimg[closeimg == 1] = 255
     return closeimg
 
-def morphgradient_binary(image,kernel=None, intk=None, n=None):
-    image[image == 255] = 1
+def morphgradient_binary(image,kernel=None, n=None):
     mg = cv2.morphologyEx(image, cv2.MORPH_GRADIENT, kernel)
     return mg
 
-def skeleton_binary(image,kernel=None, intk=None, n=None):
-    image[image == 255] = 1
+def skeleton_binary(image,kernel=None, n=None):
     done = False
     size = np.size(image)
     skel = np.zeros(image.shape,datatype)
@@ -62,37 +53,48 @@ def skeleton_binary(image,kernel=None, intk=None, n=None):
             done = True
 
     skel = cv2.morphologyEx(image, cv2.MORPH_OPEN, kernel)
-    skel[skel == 1] = 255
     return skel
 
-def holefilling_binary(image,kernel=None, intk=None, n=None):
-    image[image == 255] = 1
+def holefilling_binary(image,kernel=None, n=None):
     hf = cv2.morphologyEx(image,cv2.MORPH_OPEN,kernel)
-    hf[hf == 1] = 255
     return hf
 
-def hitormiss_binary(image,kernel=None, intk=None, n=None):
-    image[image == 255] = 1
+def hitormiss_binary(image,kernel=None, n=None):
     hm = ndimage.binary_hit_or_miss(image, structure1=n)
     return hm
 
-def tophat_binary(image,kernel=None, intk=None, n=None):
-    image[image == 255] = 1
+def tophat_binary(image,kernel=None, n=None):
     tophat = cv2.morphologyEx(image,cv2.MORPH_TOPHAT, kernel)
-    tophat[tophat == 1] = 255
     return tophat
 
-def blackhat_binary(image,kernel=None, intk=None, n=None):
-    image[image == 255] = 1
+def blackhat_binary(image,kernel=None, n=None):
     blackhat = cv2.morphologyEx(image,cv2.MORPH_BLACKHAT, kernel)
-    blackhat[blackhat == 1] = 255
     return blackhat
 
-def areafiltering_binary(image,kernel=None, intk=None, n=None):
-    image[image == 255] = 1
-    af = cv2.morphologyEx(image,cv2.MORPH_OPEN,kernel)
-    af[af == 1] = 255
-    return af
+def areafiltering_min_binary(image, kernel=None, n=None):
+    min_size = n
+    nb_components, output, stats, centroids = cv2.connectedComponentsWithStats(image, connectivity=8)
+    sizes = stats[1:, -1]; nb_components = nb_components - 1
+
+    af_min = np.zeros((image.shape))
+    for i in range(0, nb_components):
+        if sizes[i] >= min_size:
+            af_min[output == i + 1] = 1
+
+    return af_min
+
+def areafiltering_max_binary(image, kernel=None, n=None):
+    max_size = n
+    nb_components, output, stats, centroids = cv2.connectedComponentsWithStats(image, connectivity=8)
+    sizes = stats[1:, -1]; nb_components = nb_components - 1
+
+    af_max = np.zeros((image.shape))
+    for i in range(0, nb_components):
+        if sizes[i] <= max_size:
+            af_max[output == i + 1] = 1
+
+    return af_max
+
 
 if __name__=="__main__":
     # Initialize the logger
@@ -113,13 +115,16 @@ if __name__=="__main__":
                         help='The types of operations done on image in order', required=True)
     parser.add_argument('--kernelsize', dest='all_kernel', type=int,
                         help='Kernel size that should be used for all operations', required=True)
-    # parser.add_argument('--tilesize', dest='tile_size', type=int,
-    #                     help='Tile size for Image Tiling, otherwise default is 1024x1024', required=False)
     parser.add_argument('--structuringshape', dest='struct_shape', type=str,
                         help='Shape of the structuring element can either be Elliptical, Rectangular, or Cross', required=True)
 
-    parser.add_argument('--HOMarray', dest='hit_or_miss', type=str,
-                        help='Whats the array that you are trying to find', required=False)
+    # Extra arguments based on operation
+    parser.add_argument('--MinSize', dest='min_size', type=int,
+                        help='Minimum size of pixel that remains', required=False)
+    parser.add_argument('--MaxSize', dest='max_size', type=int,
+                        help='Maximum size of pixel that remains', required=False)
+    parser.add_argument('--iterations', dest='num_iterations', type=int,
+                        help='Number of Iterations to apply operation', required=False)
     
     # Input arguments
     args = parser.parse_args()
@@ -143,18 +148,22 @@ if __name__=="__main__":
         raise ValueError("Structuring Shape is not correct")
     logger.info('Structuring Shape = {}'.format(args.struct_shape))
 
-    hitormiss = args.hit_or_miss
+    min_size = args.min_size
+    max_size = args.max_size
+    iterations = args.num_iterations
+
+    if 'filter_area_with_min' in operations:
+        if min_size == None:
+            raise ValueError('Need to specify the minimum of the pixel area to keep')
+
+    if 'filter_area_with_max' in operations:
+        if max_size == None:
+            raise ValueError('Need to specify the maximum of the pixel area to keep')
+
     
-
-    if hitormiss != None:
-        hitormiss = np.array(ast.literal_eval(hitormiss))
-    hitormiss = np.array(hitormiss)
-
-    if 'hit_or_miss' in operations:
-        if hitormiss.any() == None:
-            raise ValueError("Need to specify --HOMarray integer value")
-        else:
-            logger.info('Array that we are locating: {}'.format(hitormiss))
+    if 'dilation' or 'erosion' in operations:
+        if iterations == None:
+            raise ValueError("Need to specify the number of iterations to apply the operation")
 
     # A dictionary specifying the function that will be run based on user input. 
     dispatch = {
@@ -166,26 +175,26 @@ if __name__=="__main__":
         'erosion': erode_binary,
         'skeleton': skeleton_binary,
         'fill_holes': holefilling_binary,
-        'hit_or_miss': hitormiss_binary,
-        'filter_area': areafiltering_binary,
         'top_hat': tophat_binary,
-        'black_hat': blackhat_binary
+        'black_hat': blackhat_binary,
+        'filter_area_with_min': areafiltering_min_binary,
+        'filter_area_with_max': areafiltering_max_binary
     }
 
     # Additional arguments for each function
     dict_n_args = {
-        'dilation': None,
-        'erosion': None,
-        'hit_or_miss': hitormiss,
+        'dilation': iterations,
+        'erosion': iterations,
         'invertion': None,
         'opening': None,
         'closing': None,
         'morphological_gradient': None,
         'skeleton': None,
         'fill_holes': None,
-        'filter_area': None,
         'top_hat': None,
-        'black_hat': None
+        'black_hat': None,
+        'filter_area_with_min' : min_size,
+        'filter_area_with_max' : max_size
     }
     
 
@@ -242,11 +251,13 @@ if __name__=="__main__":
                 # Images are (1, Tile_Size, Tile_Size, 1)
                 # Need to convert to (Tile_Size, Tile_Size) to be able to do operation
                 images = np.squeeze(images)
+                images[images == 255] = 1
 
                 # Initialize which function we are dispatching
                 function = dispatch[operations]
                 if callable(function):
-                    trans_image = function(images, kernel=kernel, intk=intkernel, n=dict_n_args[operations])
+                    trans_image = function(images, kernel=kernel, n=dict_n_args[operations])
+                    trans_image[trans_image==1] = 255
 
                 # The image needs to be converted back to (1, Tile_Size_Tile_Size, 1) to write it
                 reshape_img = np.reshape(trans_image[intkernel:-intkernel,intkernel:-intkernel], (1, Tile_Size, Tile_Size, 1)).astype('uint8')
