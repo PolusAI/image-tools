4dd5bcd776784e489eac629f72914c56de519da5
mmvih <mmvihani@gmail.com>
2021-04-07 16:35:53 +0000

    saves to output directory

--- polus-training-splinedist-plugin/src/main.py
+++ polus-training-splinedist-plugin/src/main.py
@@ -8,15 +8,48 @@ import bfio
 from bfio import BioReader
 
 from csbdeep.utils import normalize
+
 from splinedist import fill_label_holes
+from splinedist.utils import phi_generator, grid_generator, get_contoursize_max
+from splinedist.models import Config2D, SplineDist2D, SplineDistData2D
+from splinedist.utils import phi_generator, grid_generator, get_contoursize_max
 
-
+import cv2
 
 # Initialize the logger    
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     datefmt='%d-%b-%y %H:%M:%S')
 logger = logging.getLogger("main")
 logger.setLevel(logging.INFO)
+
+def random_fliprot(img, mask): 
+    img = np.array(img)
+    mask = np.array(mask)
+    assert img.ndim >= mask.ndim
+    axes = tuple(range(mask.ndim))
+    perm = tuple(np.random.permutation(axes))
+    img = img.transpose(perm + tuple(range(mask.ndim, img.ndim))) 
+    mask = mask.transpose(perm) 
+    for ax in axes: 
+        if np.random.rand() > 0.5:
+            img = np.flip(img, axis=ax) # reverses the order of elements
+            mask = np.flip(mask, axis=ax) # reverses the order of elements
+    return img, mask 
+
+def random_intensity_change(img):
+    img = img*np.random.uniform(0.6,2) + np.random.uniform(-0.2,0.2)
+    return img
+
+def augmenter(x, y):
+    """Augmentation of a single input/label image pair.
+    x is an input image
+    y is the corresponding ground-truth label image
+    """
+    x, y = random_fliprot(x, y)
+    x = random_intensity_change(x)
+    sig = 0.02*np.random.uniform(0,1)
+    x = x + sig*np.random.normal(0,1,x.shape)
+    return x, y
 
 def main(image_dir,
          label_dir,
@@ -48,24 +81,88 @@ def main(image_dir,
     assert collections.Counter(X_val) == collections.Counter(Y_val), "Image Test Data does not match Label Test Data for neural network"
     assert collections.Counter(X_trn) == collections.Counter(Y_trn), "Image Train Data does not match Label Train Data for neural network"
 
-    array_images_trained = np.zeros((512, 512, num_trained), dtype = 'uint8')
-    array_labels_trained = np.zeros((512, 512, num_trained), dtype = 'uint8')
+    array_images_trained = []
+    array_labels_trained = []
+
+    array_images_tested = []
+    array_labels_tested = []
+
+    axis_norm = (0,1)
+    n_channel = None
 
     for im in range(num_trained):
-
         image = os.path.join(image_dir, X_trn[im])
+        br_image = BioReader(image, max_workers=1)
+        if im == 0:
+            n_channel = br_image.shape[2]
+        im_array = br_image[:,:,0:1,0:1,0:1]
+        im_array = im_array.reshape(br_image.shape[:2])
+        array_images_trained.append(normalize(im_array,pmin=1,pmax=99.8,axis=axis_norm))
+
+    for im in range(num_tested):
+        image = os.path.join(image_dir, X_val[im])
         br_image = BioReader(image, max_workers=1)
         im_array = br_image[:,:,0:1,0:1,0:1]
         im_array = im_array.reshape(br_image.shape[:2])
-        array_images_trained[:,:,im] = normalize(im_array)
+        array_images_tested.append(normalize(im_array,pmin=1,pmax=99.8,axis=axis_norm))
+
+    contoursize_max = 0
+    logger.info("\n Getting Max Contoursize  ...")
 
     for lab in range(num_trained):
         label = os.path.join(label_dir, Y_trn[lab])
         br_label = BioReader(label, max_workers=1)
         lab_array = br_label[:,:,0:1,0:1,0:1]
         lab_array = lab_array.reshape(br_label.shape[:2])
-        array_labels_trained[:,:,lab] = fill_label_holes(lab_array)
+        array_labels_trained.append(fill_label_holes(lab_array))
 
+        obj_list = np.unique(lab_array)
+        obj_list = obj_list[1:]
+
+        for j in range(len(obj_list)):
+            mask_temp = lab_array.copy()     
+            mask_temp[mask_temp != obj_list[j]] = 0
+            mask_temp[mask_temp > 0] = 1
+
+            mask_temp = mask_temp.astype(np.uint8)    
+            contours,_ = cv2.findContours(mask_temp, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+            areas = [cv2.contourArea(cnt) for cnt in contours]    
+            max_ind = np.argmax(areas)
+            contour = np.squeeze(contours[max_ind])
+            contour = np.reshape(contour,(-1,2))
+            contour = np.append(contour,contour[0].reshape((-1,2)),axis=0)
+            contoursize_max = max(int(contour.shape[0]), contoursize_max)
+    
+    for lab in range(num_tested):
+        label = os.path.join(label_dir, Y_val[lab])
+        br_label = BioReader(label, max_workers=1)
+        lab_array = br_label[:,:,0:1,0:1,0:1]
+        lab_array = lab_array.reshape(br_label.shape[:2])
+        array_labels_tested.append(fill_label_holes(lab_array))
+
+
+    logger.info("Max Contoursize: {}".format(contoursize_max))
+
+    M = 8 # control points
+    n_params = 2 * M
+
+    grid = (2,2)
+    
+    conf = Config2D (
+    n_params        = n_params,
+    grid            = grid,
+    n_channel_in    = n_channel,
+    contoursize_max = contoursize_max,
+    )
+
+    logger.info("\n Generating phi and grids ... ")
+    phi_generator(M, conf.contoursize_max, output_dir)
+    grid_generator(M, conf.train_patch_size, conf.grid, output_dir)
+
+    model = SplineDist2D(conf, name='models', basedir=output_dir)
+    model.train(array_images_trained,array_labels_trained, validation_data=(array_images_tested, array_labels_tested), augmenter=augmenter, epochs = 1)
+
+    logger.info("\n Done Training.")
 
 if __name__ == "__main__":
     
