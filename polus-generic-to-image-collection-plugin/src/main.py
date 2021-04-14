8ff2bc3bafdf5b2fac1a1d84788d5ad60dfc3b98
Nicholas-Schaub <nick.schaub@axleinfo.com>
2021-04-14 16:02:21 -0400

    Fixed bugs, completed testing

--- polus-generic-to-image-collection-plugin/src/main.py
+++ polus-generic-to-image-collection-plugin/src/main.py
@@ -3,13 +3,24 @@ import filepattern
 import argparse, logging
 import shutil
 import typing
+from multiprocessing import cpu_count
 from pathlib import Path
+from concurrent.futures import ThreadPoolExecutor, wait
 
 # Initialize the logger
 logging.basicConfig(format='%(asctime)s - %(name)-8s - %(levelname)-8s - %(message)s',
                     datefmt='%d-%b-%y %H:%M:%S')
 logger = logging.getLogger("main")
 logger.setLevel(logging.INFO)
+
+def validate_and_copy(file: Path,
+                      outDir: Path,
+                      ) -> None:
+    
+    # Enter context manager to verify the file is a tiled tiff
+    with BioReader(file['file'],backend='python') as br:
+        
+        shutil.copy2(file['file'],outDir.joinpath(file['file'].name))
 
 def main(inpDir: Path,
          outDir: Path,
@@ -25,14 +36,25 @@ def main(inpDir: Path,
     pattern = ".*.ome.tif"
     fp = filepattern.FilePattern(inpDir,pattern)
     
-    for files in fp:
+    files = [f[0] for f in fp]
+    
+    threads = []
+    
+    with ThreadPoolExecutor(cpu_count()) as executor:
+    
+        for ind,file in enumerate(files):
+            
+            threads.append(executor.submit(validate_and_copy,file,outDir))
+           
+        done, not_done = wait(threads,timeout=0)
         
-        for file in files:    
+        logger.info('Copy progress: {:6.2f}%'.format(100*len(done)/len(threads)))
         
-            # Enter context manager to verify the file is a tiled tiff
-            with BioReader(file['file'],backend='python') as br:
-                
-                shutil.copy2(file['file'],outDir.joinpath(file['file'].name))
+        while len(not_done) > 0:
+            
+            done, not_done = wait(threads,timeout=5)
+            
+            logger.info('Copy progress: {:6.2f}%'.format(100*len(done)/len(threads)))
 
 if __name__=="__main__":
 
@@ -55,7 +77,7 @@ if __name__=="__main__":
         # switch to images folder if present
         fpath = str(Path(args.inpDir).joinpath('images').absolute())
     logger.info('inpDir = {}'.format(inpDir))
-    outDir = args.outDir
+    outDir = Path(args.outDir)
     logger.info('outDir = {}'.format(outDir))
     
     main(inpDir=inpDir,
