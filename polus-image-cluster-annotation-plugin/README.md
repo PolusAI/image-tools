2ff870b93832d9a9392da72fcc26dceec91f78ed
Nagarajan <nagarajanj2@nih.gov>
2021-06-11 15:42:11 -0400

    version 0.1.0

---
+++ polus-image-cluster-annotation-plugin/README.md
@@ -0,0 +1,39 @@
+﻿# Image cluster annotation
+
+The image cluster annotation plugin converts the original image as all zeros except at the borders which contains the cluster id. The inputs for this plugin is an image collection and csv collection. The images used as input are the images from which features were extracted using imagenet featurization plugin and clustered using hdbscan clustering plugin. The csv file in csv collection is the output from hdbscan clustering plugin. The output for this plugin is an image collection.
+
+## Inputs:
+### Input image collection:
+The input image file that need to be converted as all zeros except at the borders. This is a required parameter for the plugin.
+
+### Input csv collection:
+The input csv file that contains the cluster id. The file should be in csv format. This is a required parameter for the plugin.
+
+### Border width:
+Enter the required thickness at the borders that should be changed as cluster id.
+
+## Output:
+The output is a image file collection contains all zeros except at the borders.The border pixels will be cluster id.
+
+## Building
+
+To build the Docker image for the conversion plugin, run
+`./build-docker.sh`.
+
+## Install WIPP Plugin
+
+If WIPP is running, navigate to the plugins page and add a new plugin. Paste the contents of `plugin.json` into the pop-up window and submit.
+For more information on WIPP, visit the [official WIPP page](https://isg.nist.gov/deepzoomweb/software/wipp).
+
+## Options
+
+This plugin takes four input argument if methods other than 'Manual' is selected else three input arguments and one output argument:
+
+| Name                   | Description             | I/O    | Type   |
+|------------------------|-------------------------|--------|--------|
+| `--imgdir` | Input image collection| Input | imageCollection |
+| `--csvdir` | Input csv collection| Input | csvCollection |
+| `--borderwidth` | Enter border width | Input | integer|
+| `--outdir` | Output collection | Output | imageCollection |
+
+
---
+++ polus-image-cluster-annotation-plugin/src/main.py
@@ -0,0 +1,120 @@
+import argparse
+import logging
+import os
+import fnmatch
+import csv
+import numpy as np
+import pandas as pd
+from pathlib import Path
+from bfio import BioReader,BioWriter
+
+# Initialize the logger
+logging.basicConfig(format='%(asctime)s - %(name)-8s - %(levelname)-8s - %(message)s',
+                    datefmt='%d-%b-%y %H:%M:%S')
+logger = logging.getLogger("main")
+logger.setLevel(logging.INFO)
+
+def list_file(directory,ext):
+    """List all the files in the directory based on the extension mentioned as input.
+    
+    Args:
+        directory (str): Path to the directory containing the files.
+        
+    Returns:
+        The path to directory, list of names of the subdirectories in dirpath (if any) and the filenames of the files.
+        
+    """
+    list_of_files = [os.path.join(dirpath, file_name)
+                     for dirpath, dirnames, files in os.walk(directory)
+                     for file_name in fnmatch.filter(files, ext)]
+    return list_of_files
+   
+# Setup the argument parsing
+def main():
+    logger.info("Parsing arguments...")
+    parser = argparse.ArgumentParser(prog='main', description='Image clustering annotation plugin.')
+    parser.add_argument('--imgdir', dest='imgdir', type=str,
+                        help='Input collection- Image data', required=True)
+    parser.add_argument('--csvdir', dest='csvdir', type=str,
+                        help='Input collection- csv data', required=True)
+    parser.add_argument('--borderthickness', dest='borderthickness', type=int,
+                        help='Input collection', required=False)
+    parser.add_argument('--outdir', dest='outdir', type=str,
+                        help='Output collection', required=True)
+    
+    # Parse the arguments
+    args = parser.parse_args()  
+    
+    #Path to image directory
+    imgdir = args.imgdir
+    logger.info('imgdir = {}'.format(imgdir))
+    
+    #Path to csvfile directory
+    csvdir = args.csvdir
+    logger.info('csvdir = {}'.format(csvdir))
+    
+    #Determine k-value using different methods
+    borderthickness = args.borderthickness
+    logger.info('borderthickness = {}'.format(borderthickness))
+
+    #Path to save output image files
+    outdir = args.outdir
+    logger.info('outdir = {}'.format(outdir))
+
+    #Get list of .ome.tif files in the directory including sub folders
+    img_ext='*.ome.tif'
+    configfiles = list_file(imgdir,img_ext)
+    config = [os.path.basename(path) for path in configfiles]
+    
+    #Check whether .ome.tif files are present in the labeled image directory
+    if not configfiles:
+        raise ValueError('No .ome.tif files found.')
+
+    #Get list of .csv files in the directory including sub folders
+    csv_ext='*.csv'
+    inputcsv = list_file(csvdir,csv_ext)
+    if not inputcsv:
+        raise ValueError('No .csv files found.')
+            
+    for inpfile in inputcsv:
+        #Get the full path
+        split_file = os.path.normpath(inpfile)
+        #split to get only the filename
+        inpfilename = os.path.split(split_file)
+        file_name_csv = inpfilename[-1]
+        file_name,file_name1 = file_name_csv.split('.', 1)
+        logger.info('Reading the file ' + file_name)
+        #Read csv file
+        cluster_data = pd.read_csv(inpfile)
+        cluster_data = cluster_data.iloc[:,[0,-1]]
+        for index, row in cluster_data.iterrows():
+            filename = row[0]
+            cluster = row[1]
+            #get the image file that matches with the filename in csvfile
+            matches = [match for match in config if filename in match]
+            if len(matches) == 0:
+                logger.warning(f"Could not find image files matching the filename, {filename}. Skipping...")
+                continue
+
+            PATH = Path(imgdir)
+            PATH1 = Path(outdir)
+
+            with BioReader(PATH / filename) as br, \
+                BioWriter(PATH1 / filename,metadata=br.metadata) as bw:
+                if borderthickness:
+                    border = borderthickness
+                else:
+                    border = 2
+                #Make all pixels zero except the borders of specified thickness and assign the cluster_id to border pixels
+                mask = np.zeros(br.shape)
+                mask[:border,:]=cluster
+                mask[:,:border]=cluster
+                mask[mask.shape[0]-border:,:]=cluster
+                mask[:,mask.shape[1]-border:]=cluster
+                bw.dtype = mask.dtype
+                bw[:]=mask
+
+        logger.info("Finished all processes!")
+
+if __name__ == "__main__":
+    main()
