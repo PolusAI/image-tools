3455d98fdf3dc36c001d1c6e3201035334f774cc
Hythem Sidky <hythem.sidky@axleinfo.com>
2021-06-17 13:58:43 +0000

    Added meshing and spectral featurization

--- polus-object-spectral-featurization-plugin/src/main.py
+++ polus-object-spectral-featurization-plugin/src/main.py
@@ -1,9 +1,11 @@
-from bfio import BioReader, BioWriter
-import bioformats
-import javabridge as jutil
-import argparse, logging, subprocess, time, multiprocessing, sys
+import os
+import mesh
 import numpy as np
+import pandas as pd
+import argparse, logging
 from pathlib import Path
+from bfio import BioReader
+
 
 if __name__=='__main__':
     # Initialize the logger
@@ -21,6 +23,8 @@ if __name__=='__main__':
                         help='Input image collection to be processed by this plugin.', required=True)
     parser.add_argument('--scaleInvariant', dest='scaleInvariant', type=str,
                         help='Calculate scale invariant features.', required=True)
+    parser.add_argument('--numFeatures', dest='numFeatures', type=int, 
+                        help='Number of spectral features to calculate.', required=True)
     # Output arguments
     parser.add_argument('--outDir', dest='outDir', type=str,
                         help='Output collection', required=True)
@@ -32,32 +36,28 @@ if __name__=='__main__':
         # switch to images folder if present
         inpDir = str(Path(args.inpDir).joinpath('images').absolute())
     logger.info('inpDir = {}'.format(inpDir))
-    scaleInvariant = args.scaleInvariant == 'true'
+    scaleInvariant = args.scaleInvariant.lower() == 'true'
     logger.info('scaleInvariant = {}'.format(scaleInvariant))
+    numFeatures = args.numFeatures
+    logger.info(f'numFeatures = {numFeatures}')
     outDir = args.outDir
     logger.info('outDir = {}'.format(outDir))
     
     # Surround with try/finally for proper error catching
-    try:
-        inpDir_files = [f.name for f in Path(inpDir).iterdir() if f.is_file() and ''.join(f.suffixes[-2:]) == '.ome.tif']
-
-        # Loop through files in inpDir image collection and process
-        for i,f in enumerate(inpDir_files):
-            # Load an image
-            br = BioReader(Path(inpDir).joinpath(f))
-            image = np.squeeze(br.read_image())
-
-            # initialize the output
-            out_image = np.zeros(image.shape,dtype=br._pix['type'])
-
-            ''' Do some math and science - you should replace this '''
-            logger.info('Processing image ({}/{}): {}'.format(i,len(inpDir_files),f))
-            out_image = awesome_math_and_science_function(image)
-
-            # Write the output
-            bw = BioWriter(Path(outDir).joinpath(f),metadata=br.read_metadata())
-            bw.write_image(np.reshape(out_image,(br.num_y(),br.num_x(),br.num_z(),1,1)))
+    inpDir_files = [f.name for f in Path(inpDir).iterdir() if f.is_file() and ''.join(f.suffixes[-2:]) == '.ome.tif']
+    
+    # Loop through files in inpDir image collection and process
+    for i, f in enumerate(inpDir_files):
+        # Load an image
+        br = BioReader(Path(inpDir).joinpath(f))
         
-    finally:        
-        # Exit the program
-        sys.exit()
+        logger.info(f'Processing image ({i + 1}/{len(inpDir_files)}): {f}') 
+
+        labels, features = mesh.mesh_and_featurize_image(br, num_features=numFeatures, scale_invariant=scaleInvariant)
+
+        df = pd.DataFrame(data=labels, columns=['Label'])
+        df_data = pd.DataFrame(data=features, columns=np.arange(numFeatures).tolist())
+
+        df = pd.concat([df, df_data], axis=1)
+
+        df.to_csv(os.path.join(outDir, f'{os.path.splitext(f)[0]}.csv'), index=False)
---
+++ polus-object-spectral-featurization-plugin/src/mesh.py
@@ -0,0 +1,133 @@
+import scipy 
+import logging
+import numpy as np
+from typing import Tuple
+from bfio import BioReader
+from skimage import measure
+
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    datefmt='%d-%b-%y %H:%M:%S')
+logger = logging.getLogger('mesh')
+logger.setLevel(logging.INFO)
+
+
+def mesh_spectral_features(vertices, faces, k = 50, scale_invariant = False):
+    i = np.concatenate([faces[:,0], faces[:,0], faces[:,1], faces[:,1], faces[:,2], faces[:,2]])
+    j = np.concatenate([faces[:,1], faces[:,2], faces[:,0], faces[:,2], faces[:,0], faces[:,1]])
+    
+    idx = np.c_[i,j]
+    idx = np.unique(idx, axis=0)
+    
+    xi = vertices[idx[:,0]]
+    xj = vertices[idx[:,1]]
+    
+    dij = np.sum((xi - xj)**2, axis=1)
+    t = 2*dij.max()
+        
+    v = np.exp(-dij/t)
+    W = scipy.sparse.csr_matrix((v, (idx[:,0], idx[:,1])))
+    D = scipy.sparse.diags(np.asarray(W.sum(axis=1)).flatten(), 0)
+    L = D - W
+        
+    # Make sure we calculate enough eigenvalues. First we calculate some extra 
+    # eigenvalues in case there are many separate components. 
+    max_iter = 10
+    enough_eigvals = False
+    k_orig = k
+    n_singular = 0
+    etol = 1e-3
+    while enough_eigvals is not True and max_iter > 0:
+        
+        try:
+            # Even though we're after the smallest eigenvalues (which='SM'), we use 
+            # shift-invert mode (sigma=0) and solve for the largest. This is more efficient. 
+            # See https://docs.scipy.org/doc/scipy/reference/generated/scipy.sparse.linalg.eigsh.html
+            eigvals = scipy.sparse.linalg.eigsh(L, sigma=0, which='LM', k=k + 10, return_eigenvectors=False)
+        except RuntimeError:
+            # Runtime error is most likely due to singularity. Add small delta to matrix and try again. 
+            # This will burn through an iteration since we don't want to do this forever.
+            eps = scipy.sparse.diags(np.zeros((W.shape[0],)) + etol, 0)
+            L += eps
+            n_singular += 1
+            logger.warning('Graph laplacian likely singular. Perturbing matrix, but this may introduce inaccuracies.')
+            max_iter -= 1
+            continue
+
+        # Remove zero eigenvalues.
+        eigvals = eigvals[~np.isclose(eigvals, 0, atol=1e-5 + etol*n_singular)]
+        eigvals.sort()
+    
+        if len(eigvals) >= k_orig:
+            eigvals = eigvals[:k_orig]
+            enough_eigvals = True
+        else:
+            max_iter -= 1
+            k += k - len(eigvals) + 1 # Add one for good measure.
+
+    if max_iter <= 0:
+        logger.error('Could not solve for the desired number of eigenvalues. Please check the number of connected components in your graph.')
+    
+    if scale_invariant:
+        eigvals /= eigvals[0]
+
+    return eigvals[:k_orig]
+
+
+def mesh_and_featurize_image(
+    image: BioReader,
+    chunk_size: Tuple[int, int, int] = (256, 256, 256),
+    num_features : int = 50, 
+    scale_invariant : bool = False):
+    
+    # Store minimum and maximum bounds of every object.
+    min_bounds = {}
+    max_bounds = {}
+    
+    # Go through the image in chunks and determine extents of each ROI. 
+    for y in range(0, image.Y, chunk_size[1]):
+        for x in range(0, image.X, chunk_size[0]):
+            for z in range(0, image.Z, chunk_size[2]):
+                
+                x_step = np.min([x + chunk_size[0], image.X])
+                y_step = np.min([y + chunk_size[1], image.Y])
+                z_step = np.min([z + chunk_size[2], image.Z])
+
+                chunk = np.squeeze(image[y:y_step, x:x_step, z:z_step]) 
+                
+                labels = np.unique(chunk[chunk > 0]) 
+                
+                for label in labels:
+                    coords = np.argwhere(chunk == label)
+                    # Add a one pixel padding so long as we're not on a boundary. 
+                    curr_min = np.min(coords, axis=0) + np.array([y, x, z]) - 1
+                    curr_min = np.maximum([0, 0, 0], curr_min)
+                    
+                    curr_max = np.max(coords, axis=0) + np.array([y, x, z]) + 1
+                    curr_max = np.minimum([image.Y, image.X, image.Z], curr_max)
+                    
+                    if label not in min_bounds: 
+                        min_bounds[label] = curr_min
+                    else:
+                        min_bounds[label] = np.min(np.stack([min_bounds[label], curr_min]), axis=0)
+                    
+                    if label not in max_bounds:
+                        max_bounds[label] = curr_max
+                    else:
+                        max_bounds[label] = np.max(np.stack([max_bounds[label], curr_max]), axis=0)
+    
+
+    # Get subvolume for each ROI and generate mesh. 
+    # Note: we assume labels in min and max dicts are the same.
+    labels = list(min_bounds.keys())
+    features = np.zeros((len(labels), num_features))
+    for i, (label, min_bounds, max_bounds) in enumerate(zip(labels, min_bounds.values(), max_bounds.values())): 
+        subvol = image[min_bounds[0]:max_bounds[0], min_bounds[1]:max_bounds[1], min_bounds[2]:max_bounds[2]]
+        subvol = np.squeeze(subvol)
+
+        verts, faces, _, _ = measure.marching_cubes((subvol == label).astype(np.uint8), 0, allow_degenerate=False)
+
+        feats = mesh_spectral_features(verts, faces, k=num_features, scale_invariant=scale_invariant)
+        features[i] = feats
+
+    return labels, features
