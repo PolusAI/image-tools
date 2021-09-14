0ce705cfa8322b0888506768e51919685bb92934
Benjamin Houghton <78052721+bthoughton@users.noreply.github.com>
2021-09-14 13:29:28 -0400

    Imagej util clean (#184)
    * reupload polus imagej files
    
    * Functional revision
    
    * Functional locally, manifest validated
    
    * Add Populate.py, contians classes for parsing and storing imagej plugin data
    
    * Added functionality to parse imagj ops and generate json template for cookiecutter
    
    * Added functionality to parse imagej ops help and generate json template for cookiecutter
    
    * Add __init__.py so classes could be called
    
    * Implemented new method to parse imagej ops, update log message template
    
    * Completed first version of Populate.py
    
    * New version of Generate.py instantiates Populate.py classes to create ops
    
    * Add functionality to generate unit tests with plugins
    
    * Add unit test template and __init__.py file for importing modules during testing
    
    * Auto generated unit tests partially functional
    
    * Added auto generated unit tests and cleaned repo for PR
    
    * Initial commit of imagej-macro.py and macro file.
    
    * Remove imagej-macro files from repo
    
    * Fixed bugs from source files and added additional data type conversions - 41% plugin pass rate
    
    * Address most of the PR Imagej util clean #184 requested changes
    
    * Add tests folder for core_test.py
    
    * Update logic for determining supported ops and update template
    
    * Commit repo after addressing all applicable PR comments
    
    * Correct main.py logger to conform with the rest of plugins
    
    * Correct bug in generate.py related to generating unit test python script
    
    Co-authored-by: Anjali Taneja <at1112@georgetown.edu>
    Co-authored-by: Nicholas-Schaub <nick.schaub@axleinfo.com>

---
+++ utils/polus-imagej-util/{{cookiecutter.project_slug}}/run-plugin.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+version=$(<VERSION)
+datapath=$(readlink --canonicalize ../data)
+
+# Inputs
+{% for inp,val in cookiecutter._inputs.items() -%}
+{% if val.type == 'collection' -%}
+{{ inp }}=/data/input
+{% else -%}
+{{ inp }}=
+{% endif -%}
+{% endfor %}
+# Output paths
+{% for inp,val in cookiecutter._outputs.items() -%}
+{{ inp }}=/data/output
+{% endfor %}
+docker run --mount type=bind,source=${datapath},target=/data/ \
+            labshare/{{ cookiecutter.project_slug }}:${version} \
+            {% for inp,val in cookiecutter._inputs.items() -%}
+            --{{ inp }} {% raw %}${{% endraw %}{{ inp }}{% raw %}}{% endraw %} \
+            {% endfor -%}
+            {% for out,val in cookiecutter._outputs.items() -%}
+            --{{ out }} {% raw %}${{% endraw %}{{ out }}{% raw %}}{% endraw %}{% if not loop.last %} \{% endif %}
+            {% endfor -%}
