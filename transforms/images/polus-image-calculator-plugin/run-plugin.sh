fb3f1456a87152e661abfbf629167cad4e4a7b3f
Nicholas-Schaub <nick.schaub@axleinfo.com>
2021-09-21 17:29:27 -0400

    Updated run plugin

--- transforms/images/polus-image-calculator-plugin/run-plugin.sh
+++ transforms/images/polus-image-calculator-plugin/run-plugin.sh
@@ -4,14 +4,14 @@ version=$(<VERSION)
 datapath=$(readlink --canonicalize ../../../data)
 
 # Inputs
-primaryDir=/data/input
-primaryPattern="S1_R1_C1-C11_A1_y0(00-14)_x0(00-21)_c0{cc}.ome.tif"
+primaryDir=/data/images/MaricRatBrain2019/standard/intensity
+primaryPattern="S1_R1_C1-C11_A1_c00{c}.ome.tif"
 operator="subtract"
-secondaryDir=/data/bleedthrough/images
-secondaryPattern="original_S1_R1_C1-C11_A1_y0(00-14)_x0(00-21)_c0{cc}.ome.tif"
+secondaryDir=/data/output/images
+secondaryPattern="S1_R1_C1-C11_A1_c00{c}.ome.tif"
 
 # Output paths
-outDir=/data/output
+outDir=/data/output/subtracted
 
 # Log level, must be one of ERROR, CRITICAL, WARNING, INFO, DEBUG
 LOGLEVEL=INFO
@@ -19,7 +19,7 @@ LOGLEVEL=INFO
 docker run --mount type=bind,source=${datapath},target=/data/ \
             --user $(id -u):$(id -g) \
             --env POLUS_LOG=${LOGLEVEL} \
-            labshare/polus-image-calculator-plugin:${version} \
+            polusai/image-calculator-plugin:${version} \
             --primaryDir ${primaryDir} \
             --primaryPattern ${primaryPattern} \
             --operator ${operator} \
