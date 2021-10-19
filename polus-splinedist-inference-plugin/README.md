11b0dd81fd9b033210514b9f2d538aaaaf8d0c81
mmvih <mmvihani@gmail.com>
2021-10-19 15:14:11 +0000

    splinedist-inference-plugin

---
+++ polus-splinedist-inference-plugin/README.md
@@ -0,0 +1,33 @@
+# SplineDist Inference Plugin
+
+This WIPP plugin uses a trained SplineDist Model to make predictions on intensity based images
+This WIPP plugin is also capable of making predictions in tiles
+
+For more information on SplineDist:  
+[Published Paper](https://www.biorxiv.org/content/10.1101/2020.10.27.357640v1)  
+[Github Repository](https://github.com/uhlmanngroup/splinedist)  
+SideNote: The input images are filled.  There are no empty holes in the output predictions made with SplineDist
+
+Contact [Madhuri Vihani](madhuri.vihani@axleinfo.com) for more information.
+
+For more information on WIPP, visit the [official WIPP page](https://isg.nist.gov/deepzoomweb/software/wipp).
+
+## Building
+
+To build the Docker image for the conversion plugin, run
+`./build-docker.sh`.
+
+## Install WIPP Plugin
+
+If WIPP is running, navigate to the plugins page and add a new plugin. Paste the contents of `plugin.json` into the pop-up window and submit.
+
+## Options
+
+This plugin takes one input argument and one output argument:
+
+| Name             | Description                                                      | I/O    | Type   |
+|------------------|------------------------------------------------------------------|--------|--------|
+| `--inpImageDir`  | Path to folder with intesity based images to make predictions on | Input  | string |
+| `--inpBaseDir`   | Path to folder containing Splinedist Model's weights             | Input  | string |
+| `--imagePattern` | Filename pattern used to separate data                           | Input  | string |
+| `--outDir`       | Path to where output labelled images get saved to                | Output | string |
---
+++ polus-splinedist-inference-plugin/src/main.py
@@ -0,0 +1,70 @@
+import argparse, os 
+
+import utils
+
+from tensorflow.python.client import device_lib
+
+# Initialize the logger 
+import logging
+# Import environment variables, if POLUS_LOG empty then automatically sets to INFO
+POLUS_LOG = getattr(logging,os.environ.get('POLUS_LOG', 'INFO'))
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    datefmt='%d-%b-%y %H:%M:%S')
+logger = logging.getLogger("main")
+logger.setLevel(POLUS_LOG)
+
+def main(image_dir : str,
+         base_dir : str,
+         imagepattern : str,
+         gpu : bool,
+         output_directory : str):
+
+    utils.predict_nn(image_dir=image_dir,
+                    base_dir=base_dir,
+                    output_directory=output_directory,
+                    gpu=gpu,
+                    imagepattern=imagepattern)
+
+if __name__ == "__main__":
+    
+    logger.info("\n Parsing arguments...")
+    parser = argparse.ArgumentParser(prog='main', description='Training SplineDist')
+
+    parser.add_argument('--inpImageDir', dest='input_images_dir', type=str,
+                        help='Path to folder with intesity based images for training', required=True)
+    parser.add_argument('--inpBaseDir', dest='input_base_dir', type=str,
+                        help='Path to folder with weights for neural network', required=True)
+    parser.add_argument('--outDir', dest='output_directory', type=str,
+                        help='Path to output directory where plots are saved', required=True)
+    parser.add_argument('--imagePattern', dest='image_pattern', type=str,
+                        help='Filepattern of the images in input_images and input_labels', required=False)
+
+    # Parse the arguments
+    args = parser.parse_args()
+    image_dir = args.input_images_dir
+    base_dir = args.input_base_dir
+    output_directory = args.output_directory
+    imagepattern = args.image_pattern
+    
+    gpu = False
+    local_device_protos = device_lib.list_local_devices()
+    gpulist = [x.name for x in local_device_protos if x.device_type == 'GPU']
+    if len(gpulist) > 0:
+        gpu = True
+    
+    logger.info("Input Directory containing Images: {}".format(image_dir))
+    logger.info("Input Directory containing Parameters: {}".format(base_dir))
+    logger.info("Image Pattern: {}".format(imagepattern))
+    logger.info("Use GPU: {}".format(gpu))
+    logger.info("Output Directory to save Results: {}".format(output_directory))
+
+    if imagepattern is None:
+        imagepattern = '.*'
+
+    main(image_dir = image_dir,
+         base_dir = base_dir,
+         imagepattern = imagepattern,
+         gpu = gpu,
+         output_directory = output_directory)
+
+
