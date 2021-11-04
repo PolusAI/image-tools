ae515dbfd82b2bbc7f762eaa20216220f3e0b26f
hamshkhawar <74803092+hamshkhawar@users.noreply.github.com>
2021-11-04 12:25:38 -0500

    correction of plugin name

--- polus-image-quality-plugin/package-release.sh
+++ polus-image-quality-plugin/package-release.sh
@@ -10,7 +10,7 @@ bump2version --config-file bumpversion.cfg --new-version ${version} --allow-dirt
 ./build-docker.sh
 
 # Push to dockerhub
-docker push labshare/polus-image-quality-plugin:${version}
+docker push polusai/image-quality-plugin:${version}
 
 # Run unittests
 python -m unittest
