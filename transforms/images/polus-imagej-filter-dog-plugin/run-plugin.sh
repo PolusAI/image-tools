e89975fd5693b2c4b6505ea11ce402d45cdc2c53
Benjamin Houghton <bth51@georgetown.edu>
2022-01-03 18:40:32 +0000

    Address PR comments and re-generate plugin with UI

--- transforms/images/polus-imagej-filter-dog-plugin/run-plugin.sh
+++ transforms/images/polus-imagej-filter-dog-plugin/run-plugin.sh
@@ -4,22 +4,23 @@ version=$(<VERSION)
 datapath=$(readlink --canonicalize ../data)
 
 # Inputs
-#opName='DoGSingleSigmas'
-opName='DoGVaryingSigmas'
-inpDir=/data/input
-sigma1=2
-sigma2=3
-sigmas1=1,2
-sigmas2=1,2
+opName=
+in1=/data/input
+sigma1=
+sigma2=
+sigmas1=
+sigmas2=
 
 # Output paths
-outDir=/data/output
+out=/data/output
 
 docker run --mount type=bind,source=${datapath},target=/data/ \
-            polusai/imagej-filter-dog-plugin:${version} \
+            polusai/polus-imagej-filter-dog-plugin:${version} \
             --opName ${opName} \
-            --inpDir ${inpDir} \
+            --in1 ${in1} \
+            --sigma1 ${sigma1} \
+            --sigma2 ${sigma2} \
             --sigmas1 ${sigmas1} \
             --sigmas2 ${sigmas2} \
-            --outDir ${outDir}
+            --out ${out}
             
