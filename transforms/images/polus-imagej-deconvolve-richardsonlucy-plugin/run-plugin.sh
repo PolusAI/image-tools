4206eecc8824ba0fe28e84a257c3c23f41538c50
Benjamin Houghton <bth51@georgetown.edu>
2022-02-01 17:54:29 +0000

    Update plugin data conversion file, VERSION  and run-plugin.sh

--- transforms/images/polus-imagej-deconvolve-richardsonlucy-plugin/run-plugin.sh
+++ transforms/images/polus-imagej-deconvolve-richardsonlucy-plugin/run-plugin.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
 version=$(<VERSION)
-datapath=$(readlink --canonicalize ../data)
+datapath=$(readlink --canonicalize ../../../data)
 
 # Inputs
-opName=
+opName=RichardsonLucyC
+#opName=PadAndRichardsonLucy
 out_input=/data/input
 in1=/data/input
 in2=/data/input
-maxIterations=
+maxIterations=5
 
 # Output paths
 out=/data/output
 
 docker run --mount type=bind,source=${datapath},target=/data/ \
-            polusai/polus-imagej-deconvolve-richardsonlucy-plugin:${version} \
+            polusai/imagej-deconvolve-richardsonlucy-plugin:${version} \
             --opName ${opName} \
-            --out_input ${out_input} \
-            --in1 ${in1} \
-            --in2 ${in2} \
+            --inpDir ${in1} \
+            --psf ${in2} \
             --maxIterations ${maxIterations} \
             --out ${out}
             
