05ab05c9cecf3c646060e3b57c3dc97ef412808e
Benjamin Houghton <bth51@georgetown.edu>
2022-02-03 18:31:24 +0000

    Update the run-plugin.sh bash script

--- transforms/images/polus-imagej-deconvolve-richardsonlucytv-plugin/run-plugin.sh
+++ transforms/images/polus-imagej-deconvolve-richardsonlucytv-plugin/run-plugin.sh
@@ -1,26 +1,25 @@
 #!/bin/bash
 
 version=$(<VERSION)
-datapath=$(readlink --canonicalize ../data)
+datapath=$(readlink --canonicalize ../../../data)
 
 # Inputs
-opName=
-out_input=/data/input
-in1=/data/input
-in2=/data/input
-maxIterations=
-regularizationFactor=
+opName=PadAndRichardsonLucyTV
+inpDir=/data/input
+psf=/data/kernels
+maxIterations=5
+regularizationFactor=1
+outDir=/data/output
 
 # Output paths
 out=/data/output
 
 docker run --mount type=bind,source=${datapath},target=/data/ \
-            polusai/polus-imagej-deconvolve-richardsonlucytv-plugin:${version} \
+            polusai/imagej-deconvolve-richardsonlucytv-plugin:${version} \
             --opName ${opName} \
-            --out_input ${out_input} \
-            --in1 ${in1} \
-            --in2 ${in2} \
+            --inpDir ${inpDir} \
+            --psf ${psf} \
             --maxIterations ${maxIterations} \
             --regularizationFactor ${regularizationFactor} \
-            --out ${out}
+            --outDir ${outDir}
             
