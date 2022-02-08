37603b8627c51112bc06e0ca7495ac8849ba8088
Benjamin Houghton <bth51@georgetown.edu>
2022-02-08 16:13:26 +0000

    Update the run-plugin.sh bash script with new variable names

--- transforms/images/polus-imagej-filter-correlate-plugin/run-plugin.sh
+++ transforms/images/polus-imagej-filter-correlate-plugin/run-plugin.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
 
 version=$(<VERSION)
-datapath=$(readlink --canonicalize ../data)
+datapath=$(readlink --canonicalize ../../../data)
 
 # Inputs
-opName=
-out_input=/data/input
-in1=/data/input
-in2=/data/input
+#opName=PadAndCorrelateFFT
+opName=CorrelateFFTC
+inpDir=/data/input
+kernel=/data/kernels
 
 # Output paths
-out=/data/output
+outDir=/data/output
 
 docker run --mount type=bind,source=${datapath},target=/data/ \
-            polusai/polus-imagej-filter-correlate-plugin:${version} \
+            polusai/imagej-filter-correlate-plugin:${version} \
             --opName ${opName} \
-            --out_input ${out_input} \
-            --in1 ${in1} \
-            --in2 ${in2} \
-            --out ${out}
+            --inpDir ${inpDir} \
+            --kernel ${kernel} \
+            --outDir ${outDir}
             
