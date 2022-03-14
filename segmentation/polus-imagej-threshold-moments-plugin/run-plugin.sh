b084f38c23879a5f3e8daeae3013d91ec6d71709
Benjamin Houghton <bth51@georgetown.edu>
2022-03-14 18:28:14 +0000

    Update the imagej-threshold-moments-plugin docker bash scripts and plugin manifest

--- segmentation/polus-imagej-threshold-moments-plugin/run-plugin.sh
+++ segmentation/polus-imagej-threshold-moments-plugin/run-plugin.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 version=$(<VERSION)
-datapath=$(readlink --canonicalize ../data)
+datapath=$(readlink --canonicalize ../../data)
 
 # Inputs
-opName=
-in1=/data/input
+opName='ApplyThresholdMethod$Moments'
+inpDir=/data/input
 
 # Output paths
-out=/data/output
+outDir=/data/output
 
 docker run --mount type=bind,source=${datapath},target=/data/ \
-            polusai/polus-imagej-threshold-moments-plugin:${version} \
+            polusai/imagej-threshold-moments-plugin:${version} \
             --opName ${opName} \
-            --in1 ${in1} \
-            --out ${out}
+            --inpDir ${in1} \
+            --outDir ${out}
             
