85038520ff6e54c50280de70b97f9b741ef04e3f
Benjamin Houghton <bth51@georgetown.edu>
2022-03-21 14:52:41 +0000

    Update the imagej-threshold-shanbhag-plugin manifest, README, bash docker scripts and src/main.py

--- segmentation/polus-imagej-threshold-shanbhag-plugin/run-plugin.sh
+++ segmentation/polus-imagej-threshold-shanbhag-plugin/run-plugin.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 version=$(<VERSION)
-datapath=$(readlink --canonicalize ../data)
+datapath=$(readlink --canonicalize ../../data)
 
 # Inputs
-opName=
-in1=/data/input
+opName='ApplyThresholdMethod$Shanbhag'
+inpDir=/data/input
 
 # Output paths
-out=/data/output
+outDir=/data/output
 
 docker run --mount type=bind,source=${datapath},target=/data/ \
-            polusai/polus-imagej-threshold-shanbhag-plugin:${version} \
+            polusai/imagej-threshold-shanbhag-plugin:${version} \
             --opName ${opName} \
-            --in1 ${in1} \
-            --out ${out}
+            --inpDir ${inpDir} \
+            --outDir ${outDir}
             
