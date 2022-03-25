e87a37dbe427bb35f2934c2e1f1ed47d1083ef62
Benjamin Houghton <bth51@georgetown.edu>
2022-03-25 13:31:40 +0000

    Update the imagej-threshold-triangle-plugin manifest, description and docker bash scripts

--- segmentation/polus-imagej-threshold-triangle-plugin/build-docker.sh
+++ segmentation/polus-imagej-threshold-triangle-plugin/build-docker.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 version=$(<VERSION)
-docker build . -t polusai/polus-imagej-threshold-triangle-plugin:${version}
+docker build . -t polusai/imagej-threshold-triangle-plugin:${version}
