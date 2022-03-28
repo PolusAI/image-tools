51aee8f44acdaf485931025ad07d4232f87d8f35
Benjamin Houghton <bth51@georgetown.edu>
2022-03-28 15:10:03 +0000

    Commit imagej-transform-flatiterableview-plugin after UI generation

---
+++ transforms/polus-imagej-transform-flatiterableview-plugin/run-plugin.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+version=$(<VERSION)
+datapath=$(readlink --canonicalize ../data)
+
+# Inputs
+opName=
+in1=/data/input
+
+# Output paths
+out=/data/output
+
+docker run --mount type=bind,source=${datapath},target=/data/ \
+            polusai/polus-imagej-transform-flatiterableview-plugin:${version} \
+            --opName ${opName} \
+            --in1 ${in1} \
+            --out ${out}
+            
