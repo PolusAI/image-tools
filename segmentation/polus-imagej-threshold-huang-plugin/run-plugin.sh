83d349f273a815c3a24d0a7b63cafd82fb4d0ee9
Benjamin Houghton <78052721+bthoughton@users.noreply.github.com>
2022-05-03 13:07:33 -0400

    ImageJ Threshold Huang (#297)
    * reupload polus imagej files
    
    * Functional revision
    
    * Functional locally, manifest validated
    
    * Add Populate.py, contians classes for parsing and storing imagej plugin data
    
    * Added functionality to parse imagj ops and generate json template for cookiecutter
    
    * Added functionality to parse imagej ops help and generate json template for cookiecutter
    
    * Add __init__.py so classes could be called
    
    * Implemented new method to parse imagej ops, update log message template
    
    * Completed first version of Populate.py
    
    * New version of Generate.py instantiates Populate.py classes to create ops
    
    * Add functionality to generate unit tests with plugins
    
    * Add unit test template and __init__.py file for importing modules during testing
    
    * Auto generated unit tests partially functional
    
    * Added auto generated unit tests and cleaned repo for PR
    
    * Initial commit of imagej-macro.py and macro file.
    
    * Remove imagej-macro files from repo
    
    * Fixed bugs from source files and added additional data type conversions - 41% plugin pass rate
    
    * Updated bfio, filepattern, and base container
    
    * Address most of the PR Imagej util clean #184 requested changes
    
    * Add tests folder for core_test.py
    
    * Update logic for determining supported ops and update template
    
    * Commit repo after addressing all applicable PR comments
    
    * Correct main.py logger to conform with the rest of plugins
    
    * Correct bug in generate.py related to generating unit test python script
    
    * Commit imagej-filter-sobel-plugi for WIPP testing
    
    * Commit imagej-filter-sobel-plugin
    
    * Commit imagej-image-integral-plugin
    
    * Remove image-integral and filter-dog plugins for PR, which have been moved to new branches
    
    * Bump the version for imagej plugin generation pipeline
    
    * Fix bug associated with changing imagej-instance, ij, to non-global variable
    
    * First version of imagej-ui dashboard
    
    * Update version numbers in filepattern.cfg and bfio.cfg
    
    * Address all applicable PR comments
    
    * Remove __init__.py from src dir, and add src dir to system path in unit_test.py
    
    * Correct src dir path variable name in unit_test.py
    
    * Add python black to pipeline
    
    * Correct bug where json list converted to string with imagej  ui
    
    * Modify generate.py and populate.py to function independently
    
    * Add python black to plugin pipeline
    
    * Modify jinja logic to use updated variable names from UI dashboard
    
    * Correct UI dashboard logic to strip out quotations from strings
    
    * Removed logs from git
    
    * Merge polus-plugins/dev with imagej-util-clean
    
    * Commit updated VERSION
    
    * Integrate plugin generation and containerization with dashboard UI
    
    * Update ImageJ plugin UI comments
    
    * Update UI output message
    
    * Update README.md with v0.3.0 imagej pipeline info
    
    * Update generate.py with merge conflict changes
    
    * Remove gutter decorators from resolved merge conflicts
    
    * Update .gitignore to ignore ipynb checkpoints
    
    * Update generated README.md with plugin description
    
    * Update the I/O titles in generated README.md
    
    * Correct bug in UI where whitespace added to strings in list
    
    * Update jinja logic to handle WIPP arrays
    
    * Update logic to not strip out '[]' from data types like double[]
    
    * Fix bug that added extra parentheses to list due to last commit
    
    * Add debug logger and remove commented out code
    
    * Upload requirement.txt for plugin pipeline
    
    * Update dependencies section in README.md
    
    * Remove openjdk==8 from requirements.txt
    
    * Update install dependencies command in README.md
    
    * Commit the auto-generated imagej-threshold-huang plugin
    
    * Correct syntax error in source, update docker bash scripts and plugin manifest
    
    * Commit the auto-generated imagej-threshold-huang plugin
    
    * Correct syntax error in source, update docker bash scripts and plugin manifest
    
    * Update syntax error in run-plugin.sh and update plugin description in README.md
    
    Co-authored-by: Anjali Taneja <at1112@georgetown.edu>
    Co-authored-by: Nicholas-Schaub <nick.schaub@axleinfo.com>

---
+++ segmentation/polus-imagej-threshold-huang-plugin/run-plugin.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+version=$(<VERSION)
+datapath=$(readlink --canonicalize ../../data)
+
+# Inputs
+opName='ApplyThresholdMethod$Huang'
+inpDir=/data/input
+outDir=/data/output
+
+# Output paths
+out=/data/output
+
+docker run --mount type=bind,source=${datapath},target=/data/ \
+            polusai/imagej-threshold-huang-plugin:${version} \
+            --opName ${opName} \
+            --inpDir ${inpDir} \
+            --outDir ${outDir}
+            
