827cc9fd304c1fe96478ea581d4fee8659df22f9
Benjamin Houghton <bth51@georgetown.edu>
2022-05-06 14:45:49 +0000

    Use VERSION file to populate cookiecutter templates and ensure JVM is always shut down

--- utils/polus-imagej-util/classes/populate.py
+++ utils/polus-imagej-util/classes/populate.py
@@ -1042,22 +1042,30 @@ if __name__ == '__main__':
         # Save a directory for the cookietin json files
         cookietin_path = cwd.joinpath('utils/polus-imagej-util/cookietin')
         
+        # Get the pipeline VERSION
+        version_path = Path(__file__).parents[1].joinpath('VERSION')
+        with open(version_path, 'r') as fhand:
+            version = next(fhand)
+        
         # Build the json dictionary to be passed to the cookiecutter module 
         populater.build_json(
             'Benjamin Houghton', 
             'benjamin.houghton@axleinfo.com', 
             'bthoughton', 
-            '0.5.0', 
+            version, 
             cookietin_path)
+    
+    finally:
         
         print('Shutting down JVM\n')
         
         # Remove the imagej instance
         del populater._ij
-    
-    finally:
+        
         # Shut down JVM
         jpype.shutdownJVM()
+    
+    print('Updating templates with previously genreated ops')
     
     # Instantiate the generated ops parser and update templates with manifests
     parser = GeneratedParser()
