ed090d433b0b5d56d9d6eb6d65bcfac9c3ccc536
Benjamin Houghton <bth51@georgetown.edu>
2022-06-02 10:32:32 +0000

    Update ImageJ pipeline to scale FFT filter ops

--- utils/polus-imagej-util/{{cookiecutter.project_slug}}/src/main.py
+++ utils/polus-imagej-util/{{cookiecutter.project_slug}}/src/main.py
@@ -59,7 +59,72 @@ def create_histogram(path, ij):
     
     return histogram
 {% endif -%}
+{% if cookiecutter.scalability == 'fft-filter' %}
+def pad_image(fp, y, x, padding_size):
+
+    # Get the file path to the image to pad
+    img_path = fp.get_matching(Y=y, X=x)[0]['file']
+    print(img_path)
+    
+    print('Reading {} - position x:{} y:{}'.format(img_path.name, x, y))
+    
+    # Read the image to pad
+    br = BioReader(img_path, backend='python')
+    meta = br.metadata
+    img = br[:]
+    br.close()
+    original_shape = img.shape
+    print(original_shape)
+    
+    # Pad the image with symmetric reflection
+    padded_img = np.pad(img, original_shape, mode='symmetric')
+
+    # Iterate over each row of tiles
+    for r in range(-1, 2):
         
+        # Define the current y (row) to read
+        Y = y + r
+        
+        # Iterate over each column in the current row
+        for c in range(-1, 2):
+            
+            # Define the current x (column) to read
+            X = x + c
+            
+            # Try to get file path to the current image in the 3x3 grid
+            next_image = fp.get_matching(X=X, Y=Y)
+            
+            if len(next_image) == 0:
+                print('No image in position y:{}, x:{}'.format(Y,X))
+            
+            else:
+                print(next_image)
+                
+                # Read the next image
+                br = BioReader(Path(next_image[0]['file']))
+                img = br[:]
+                br.close()
+                
+                # Define the current tiles array indices
+                y1 = (r + 1)*original_shape[0]
+                y2 = y1 + original_shape[0]
+                x1 = (c + 1)*original_shape[0]
+                x2 = x1 + original_shape[0]
+                
+                print('y1:', y1)
+                print('y2:', y2)
+                print('x1:', x1)
+                print('x2:', x2)
+                
+                # Slice in the current image
+                padded_img[y1:y2, x1:x2] = img
+    
+    # Define the final indices of the paddded image
+    i1 = original_shape[0] - padding_size
+    i2 = original_shape[0]*2 + padding_size
+    
+    return padded_img[i1:i2, i1:i2], original_shape, meta
+{% endif -%}
 
 # Import environment variables
 POLUS_LOG = getattr(logging,os.environ.get('POLUS_LOG','INFO'))
@@ -83,6 +148,9 @@ def main({#- Required inputs -#}
          {%         endif -%}
          {%     endif -%}
          {% endfor -%}
+         {%- if cookiecutter.scalability == 'fft-filter' -%}
+         _pattern: str,
+         {%- endif %}
          {#- Required Outputs (all outputs are required, and should be a Path) -#}
          {% for inp,val in cookiecutter._outputs.items() -%}
          _{{ inp }}: Path,
@@ -143,16 +211,43 @@ def main({#- Required inputs -#}
         if not _{{ inp }}.is_dir():
             raise FileNotFoundError('The {} collection directory does not exist'.format(_{{ inp }}))
         
+        # Add the list of images to the arguments (images) list
+        # There will be a single list for each collection input within args list
+        {%- if cookiecutter.scalability == 'fft-filter' and val.title in ['in1', 'inpDir'] %}
+        
+        # Instantiate the filepatter object
+        {{ inp }}_fp = filepattern.FilePattern(_{{inp}}, _pattern)
+        
+        # Add the list of images to the arguments (images) list
+        # There will be a single list for each collection input within args list
+        args.append([f[0] for f in {{ inp }}_fp() if f[0]['file'].is_file()])
+        if arg_len == 0:
+            arg_len = len(args[-1])
+        
+        {%- elif cookiecutter.scalability == 'fft-filter' and val.title in ['in2', 'kernel'] %}
+        
         # Infer the file pattern of the collection
         pattern_guess = filepattern.infer_pattern(_{{inp}}.iterdir())
         
         # Instantiate the filepatter object
-        fp = filepattern.FilePattern(_{{inp}}, pattern_guess)
+        {{ inp }}_fp = filepattern.FilePattern(_{{inp}}, pattern_guess)
+        
+        {{ inp }}_path = [f[0]['file'] for f in {{ inp }}_fp() if f[0]['file'].is_file()]
+        
+        {% else %}
+        # Infer the file pattern of the collection
+        pattern_guess = filepattern.infer_pattern(_{{inp}}.iterdir())
+        
+        # Instantiate the filepatter object
+        {{ inp }}_fp = filepattern.FilePattern(_{{inp}}, pattern_guess)
         
         # Add the list of images to the arguments (images) list
         # There will be a single list for each collection input within args list
-        args.append([f[0]['file'] for f in fp() if f[0]['file'].is_file()])
-        arg_len = len(args[-1])
+        args.append([f[0]['file'] for f in {{ inp }}_fp() if f[0]['file'].is_file()])
+        if arg_len == 0:
+            arg_len = len(args[-1])
+        {% endif %}
+        
     else:
         argument_types.append(None)
         args.append([None])
@@ -169,7 +264,7 @@ def main({#- Required inputs -#}
     
     # This ensures each input collection has the same number of images
     # If one collection is a single image it will be duplicated to match length
-    # of the other input collection
+    # of the other input collection - only works when 1 input is a collection
     for i in range(len(args)):
         if len(args[i]) == 1:
             args[i] = args[i] * arg_len
@@ -204,7 +299,7 @@ def main({#- Required inputs -#}
                 dtype = ij.py.dtype({{ inp }})
                 # Save the shape for out input
                 shape = ij.py.dims({{ inp }})
-            {%- endif %}
+                {%- endif %}
             {%- endif %}{% endfor %}
             
             {%- for inp,val in cookiecutter._inputs.items() if val.type != 'collection' and inp != 'opName' and inp != 'out_input' %}
@@ -326,13 +421,108 @@ def main({#- Required inputs -#}
         jpype.shutdownJVM()
         logger.info('JVM shutdown complete')
     
-    {% else %}
+    {% elif cookiecutter.scalability == 'fft-filter' %}
     
+    # Attempt to convert inputs to java types and run the filter op
+    try:
+        
+        {%- for inp,val in cookiecutter._inputs.items() if val.type != 'collection' and inp != 'opName' and inp != 'out_input' %}
+            if _{{ inp }} is not None:
+                {{ inp }} = ij_converter.to_java(ij, _{{ inp }},{{ inp }}_types[_opName],dtype)
+        {% endfor %}
+        
+        {%- for inp,val in cookiecutter._inputs.items() if val.title in ['in2', 'kernel'] and val.type=='collection' %}
+        # Load the kernel image
+        logger.info('Loading image: {}'.format({{ inp }}_path[0]))
+        {{ inp }}_br = BioReader({{ inp }}_path[0])
+        
+        # Convert to appropriate numpy array
+        {{ inp }} = ij_converter.to_java(ij, np.squeeze({{ inp }}_br[:,:,0:1,0,0]),{{ inp }}_type)
+        {{ inp }}_br.close()
+        
+        kernel_shape = ij.py.dims({{ inp }})
+        {% endfor %}
+        
+        for ind, (
+            {%- for inp,val in cookiecutter._inputs.items() if val.type=='collection' and inp not in ['out_input', 'in2', 'kernel'] -%}
+            {{ inp }}_path,
+            {%- endfor %}) in enumerate(zip(*args)):
+            
+            {%- for inp,val in cookiecutter._inputs.items() if val.type=='collection' and inp not in ['out_input', 'in2', 'kernel'] %}
+            if {{ inp }}_path != None:
+                
+                {%- if loop.first %}
+                # Load the first plane of image in {{ inp }} collection
+                logger.info('Processing image: {}'.format({{ inp }}_path))
+                
+                # Define x and y spatial position of current tile
+                x = {{ inp }}_path['x']
+                y = {{ inp }}_path['y']
+                
+                # Save input collection file name and data type
+                fname = {{ inp }}_path['file'].name
+                
+                # Pad the tile
+                padded_img, orginal_shape, metadata = pad_image(
+                    fp={{ inp }}_fp, y=y, x=x, padding_size=kernel_shape[0]
+                    )
+                
+                # Convert to appropriate numpy array
+                {{ inp }} = ij_converter.to_java(ij, padded_img,{{ inp }}_type)
+                
+                # Save the shape and data type for out input array
+                shape = ij.py.dims({{ inp }})
+                dtype = ij.py.dtype({{ inp }})
+                {%- endif %}
+            {% endfor %}
+            
+            # Generate the out input variable if required
+            {%- for inp,val in cookiecutter._inputs.items() if inp == 'out_input' %}
+            {{ inp }} = ij_converter.to_java(ij, np.zeros(shape=shape, dtype=dtype), 'IterableInterval')
+            {% endfor %}
+            
+            logger.info('Running op...')
+            {% for i,v in cookiecutter.plugin_namespace.items() %}
+            {%- if loop.first %}if{% else %}elif{% endif %} _opName == "{{ i }}":
+                {{ v }}
+            {% endfor %}
+            logger.info('Completed op!')
+            
+            {% for out,val in cookiecutter._outputs.items() -%}
+            # Saving output file to {{ out }}
+            logger.info('Saving...')
+            {{ out }}_array = ij_converter.from_java(ij, {{ out }},{{ out }}_types[_opName])
+            
+            
+            # Define padding indices to trim
+            i1 = kernel_shape[0]
+            i2 = orginal_shape[0] + kernel_shape[0]
+            
+            {{ out }}_array = {{ out }}_array[i1:i2, i1:i2]
+            
+            bw = BioWriter(_{{ out }}.joinpath(fname),metadata=metadata)
+            bw.Z = 1
+            bw.dtype = {{ out }}_array.dtype
+            bw[:] = {{ out }}_array.astype(bw.dtype)
+            bw.close()
+            {%- endfor %}
+            
+    except:
+        logger.error('There was an error, shutting down jvm before raising...')
+        raise
+            
+    finally:
+        # Exit the program
+        logger.info('Shutting down jvm...')
+        del ij
+        jpype.shutdownJVM()
+        logger.info('Complete!')
+    
+    {% else %}
     # If plugin scale type is not defined
     logger.info('Plugin scale type not developed, shutting down jvm without running op...')
     del ij
     jpype.shutdownJVM()
-    
     {% endif %}
 
 if __name__=="__main__":
@@ -351,6 +541,10 @@ if __name__=="__main__":
     parser.add_argument('--{{ val.title }}', dest='{{ out }}', type=str,
                         help='{{ val.description }}', required=True)
     {% endfor %}
+    {%- if cookiecutter.scalability == 'fft-filter' %}
+    parser.add_argument('--pattern', dest='pattern', type=str,
+                        help='Input collection file pattern', required=True)
+    {% endif %}
     """ Parse the arguments """
     args = parser.parse_args()
     
@@ -365,6 +559,10 @@ if __name__=="__main__":
     {% endif -%}
     logger.info('{{ val.title }} = {}'.format(_{{ inp }}))
     {% endfor %}
+    {%- if cookiecutter.scalability == 'fft-filter' %}
+    _pattern = args.pattern
+    logger.info('pattern = {}'.format(_pattern))
+    {% endif %}
     # Output Args
     {%- for out,val in cookiecutter._outputs.items() %}
     _{{ out }} = Path(args.{{ out }})
@@ -376,6 +574,9 @@ if __name__=="__main__":
     {%- for inp,val in cookiecutter._inputs.items() if inp != 'out_input' -%}
     _{{ inp }}=_{{ inp }},
     {% endfor -%}
+    {%- if cookiecutter.scalability == 'fft-filter' %}
+    _pattern = _pattern,
+    {% endif -%}
     {%- for inp,val in cookiecutter._outputs.items() -%}
     _{{ inp }}=_{{ inp }}{% if not loop.last %},{% endif %}{% endfor %}{% endfilter -%}
     )
