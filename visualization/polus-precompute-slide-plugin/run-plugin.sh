cb96f92ec070ba18c9d903e9eb0ca5d570f351cf
Benjamin Houghton <bth51@georgetown.edu>
2022-07-21 13:19:40 +0000

    Update precompute-plugin docker file entry point and run-plugin.sh script

--- visualization/polus-precompute-slide-plugin/run-plugin.sh
+++ visualization/polus-precompute-slide-plugin/run-plugin.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 version=$(<VERSION)
-datapath=$(readlink --canonicalize ../data)
+datapath=$(readlink --canonicalize ../../data)
 echo ${datapath}
 
 # Inputs
 inpDir=/data/input
-pyramidType=Neuroglancer
+pyramidType=Zarr
 imageType=image
+filePattern="p02_x(01-24)_y(01-16)_wx(0-2)_wy(0-2)_c{c}.ome.tif"
 
 # Output paths
 outDir=/data/output
@@ -17,5 +18,6 @@ docker run --mount type=bind,source=${datapath},target=/data/ \
             labshare/polus-precompute-slide-plugin:${version} \
             --inpDir ${inpDir} \
             --pyramidType ${pyramidType} \
+            --filePattern ${filePattern} \
             --imageType ${imageType} \
             --outDir ${outDir}
