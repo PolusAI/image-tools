61c75d02672de97ab2b2a6f8d2008d87610caa82
hamshkhawar <hamdahshafqat@hotmail.com>
2022-12-14 12:04:05 -0600

    fixing unittest bug

--- features/nyxus-plugin/tests/test_main.py
+++ features/nyxus-plugin/tests/test_main.py
@@ -1,19 +1,17 @@
-import numpy as np
 import os, sys, unittest
 from filepattern import FilePattern
 
 dirpath = os.path.abspath(os.path.join(__file__, "../.."))
 sys.path.append(dirpath)
-import re
-import numpy as np
 from typing import List
 from src.func import nyxus_func
-import pathlib
 import csv
 from preadator import ProcessManager
+from multiprocessing import cpu_count
+
 
 inpDir = os.path.join(dirpath, "data/intensity")
-segDir = os.path.join(dirpath, "data/segmentation")
+segDir = os.path.join(dirpath, "data/label")
 out_dir = os.path.join(dirpath, "data/out")
 intPattern = "p00{z}_x{x+}_y{y+}_wx{t}_wy{p}_c2.ome.tif"
 segPattern = "p00{z}_x{x+}_y{y+}_wx{t}_wy{p}_c1.ome.tif"
@@ -34,6 +32,9 @@ class Test_nyxus_func(unittest.TestCase):
         self.pixelPerMicron = pixelPerMicron
 
     def test_nyxus_func(self):
+
+        ProcessManager.num_processes(max([cpu_count(), 2]))
+        ProcessManager.init_processes(name="Nyxus")
         int_images = FilePattern(self.inpDir, self.intPattern)
         seg_images = FilePattern(self.segDir, self.segPattern)
 
@@ -45,21 +46,34 @@ class Test_nyxus_func(unittest.TestCase):
             int_file = [i["file"] for i in i_image]
             seg_file = s_image[0]["file"]
 
-            nyxus_func(
+            ProcessManager.submit_process(
+                nyxus_func,
                 int_file,
                 seg_file,
                 self.out_dir,
                 self.features,
-                self.neighborDist,
                 self.pixelPerMicron,
+                self.neighborDist,
             )
 
-        csvfilename = [f for f in os.listdir(self.outDir) if "csv" in f][0]
-        # self.assertTrue(replicate in csvfilename)
-        # with open(pathlib.Path(self.outDir).joinpath(csvfilename), "r") as csvfile:
-        #     csvfile = csv.reader(csvfile)
-        #     file_p = [row[0] for row in csvfile if row is not None]
-        #     self.assertFalse(file_p[1:] == None)
+        ProcessManager.join_processes()
+
+        self.assertTrue(len(os.listdir(self.out_dir)) == len(os.listdir(self.inpDir)))
+        csvfilename = [f for f in os.listdir(self.out_dir) if "csv" in f][0]
+        with open(os.path.join(self.out_dir, csvfilename), "r") as csvfile:
+            csvfile = csv.reader(csvfile)
+
+            int_images = []
+            seg_images = []
+
+            for row in csvfile:
+                seg_images.append(row[0])
+
+            for row in csvfile:
+                int_images.append(row[1])
+
+            self.assertTrue([f for f in seg_images if "c1" in f] != None)
+            self.assertFalse([f for f in int_images if "c1" in f] == None)
 
 
 if __name__ == "__main__":
