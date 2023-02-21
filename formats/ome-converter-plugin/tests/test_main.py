8cf67ef2acbbf2cbd24866a187d12740f86ed005
hamshkhawar <hamdahshafqat@hotmail.com>
2023-02-21 13:17:54 -0600

    isort testing

--- formats/ome-converter-plugin/tests/test_main.py
+++ formats/ome-converter-plugin/tests/test_main.py
@@ -5,6 +5,11 @@ import sys
 
 import filepattern as fp
 
+from polus.plugins.formats import ome_converter
+
+# from src.polus.plugins.formats.ome_converter.image_converter import \
+#     image_converter
+
 dirpath = os.path.abspath(os.path.join(__file__, "../.."))
 sys.path.append(dirpath)
 
@@ -18,6 +23,9 @@ filePattern = "p0{z}_x{x+}_y{y+}_wx{t}_wy{p}_c{c}.ome.tif"
 def test_image_converter_omezarr():
     """Testing of bioformat supported image datatypes conversion to ome.zarr file format."""
     pattern = ".*"
+    fileExtension = ".ome.zarr"
     fps = fp.FilePattern(inpDir, pattern)
     for file in fps():
-        file[1][0]
+        fl = file[1][0]
+        ome_converter(pathlib.Path(fl), ".ome.zarr", outDir)
+    assert all([f for f in os.listdir(outDir) if fileExtension in f]) is True
