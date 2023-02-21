1aba9505baf49058ef3a5cb0ad5f8c3d0be63a60
hamshkhawar <hamdahshafqat@hotmail.com>
2023-02-21 12:08:27 -0600

    fix isort

--- formats/ome-converter-plugin/tests/test_main.py
+++ formats/ome-converter-plugin/tests/test_main.py
@@ -3,12 +3,21 @@ import os
 import pathlib
 import sys
 
+import filepattern as fp
+
 dirpath = os.path.abspath(os.path.join(__file__, "../.."))
 sys.path.append(dirpath)
-
 
 inpDir = pathlib.Path(dirpath, "data/input")
 outDir = pathlib.Path(dirpath, "data/out")
 if not outDir.exists():
     outDir.mkdir(exist_ok=True, parents=True)
 filePattern = "p0{z}_x{x+}_y{y+}_wx{t}_wy{p}_c{c}.ome.tif"
+
+
+def test_image_converter_omezarr():
+    """Testing of bioformat supported image datatypes conversion to ome.zarr file format."""
+    pattern = ".*"
+    fps = fp.FilePattern(inpDir, pattern)
+    for file in fps():
+        file[1][0]
