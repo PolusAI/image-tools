25f71a480c57355ef5797755b7937d6192731fe2
hamshkhawar <hamdahshafqat@hotmail.com>
2023-05-15 13:41:16 -0500

    scalability pytest for nyxus

--- features/nyxus-plugin/tests/test_main.py
+++ features/nyxus-plugin/tests/test_main.py
@@ -100,6 +100,64 @@ def test_nyxus_func(synthetic_images, output_directory, get_params) -> None:
     assert vdf.shape is not None
 
 
+@pytest.fixture(params=[5000, 10000, 20000, 30000])
+def scaled_sizes(request):
+    """To get the parameter of the fixture."""
+    yield request.param
+
+
+@pytest.fixture
+def scaled_images(
+    inp_dir, seg_dir, scaled_sizes
+) -> Generator[Tuple[pathlib.Path, pathlib.Path], None, None]:
+    """Generate random synthetic images."""
+    im = np.zeros((scaled_sizes, scaled_sizes))
+    points = scaled_sizes * np.random.random((2, 1**2))
+    im[(points[0]).astype(int), (points[1]).astype(int)] = 1
+    im = filters.gaussian(im, sigma=scaled_sizes / (20.0 * 10))
+    blobs = im > im.mean()
+    lab_blobs = measure.label(blobs, background=0)
+    intname = "y04_r1_c1.ome.tif"
+    segname = "y04_r1_c0.ome.tif"
+    int_name = pathlib.Path(inp_dir, intname)
+    seg_name = pathlib.Path(seg_dir, segname)
+    io.imsave(int_name, im)
+    io.imsave(seg_name, lab_blobs)
+    yield inp_dir, seg_dir
+
+
+@pytest.fixture(params=[(".csv", "MEAN")])
+def get_scaled_params(request):
+    """To get the parameter of the fixture."""
+    yield request.param
+
+
+def test_scaled_nyxus_func(scaled_images, output_directory, get_scaled_params) -> None:
+    """Test Nyxus Function.
+
+    This unit test runs the nyxus function and validates the outputs
+    """
+    inp_dir, seg_dir = scaled_images
+    int_pattern = "y04_r{r:d}_c1.ome.tif"
+    seg_pattern = "y04_r{r:d}_c0.ome.tif"
+    int_images = fp.FilePattern(inp_dir, int_pattern)
+    seg_images = fp.FilePattern(seg_dir, seg_pattern)
+    fileext, feat = get_scaled_params
+    for s_image in seg_images:
+        i_image = int_images.get_matching(**{k: v for k, v in s_image[0].items()})
+        nyxus_func(
+            int_file=i_image[0][1],
+            seg_file=s_image[1],
+            out_dir=output_directory,
+            features=[feat],
+            file_extension=fileext,
+        )
+    output_ext = [f.suffix for f in output_directory.iterdir()][0]
+    assert output_ext == fileext
+    vdf = vaex.open([f for f in output_directory.iterdir()][0])
+    assert vdf.shape is not None
+
+
 def test_cli(synthetic_images, output_directory, get_params) -> None:
     """Test Cli."""
     inp_dir, seg_dir = synthetic_images
