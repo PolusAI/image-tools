6391d452bda66bc2738682428386982e6d383a72
Camilo Velez <camilov61@gmail.com>
2023-05-16 21:47:18 -0400

    fix: fixed unexported load_config function

--- src/polus/plugins/_plugins/classes/__init__.py
+++ src/polus/plugins/_plugins/classes/__init__.py
@@ -1,4 +1,5 @@
 """Plugin classes and functions."""
+<<<<<<< rm1
 from polus.plugins._plugins.classes.plugin_classes import ComputePlugin
 from polus.plugins._plugins.classes.plugin_classes import Plugin
 from polus.plugins._plugins.classes.plugin_classes import _load_plugin
@@ -8,6 +9,20 @@ from polus.plugins._plugins.classes.plugin_classes import refresh
 from polus.plugins._plugins.classes.plugin_classes import remove_all
 from polus.plugins._plugins.classes.plugin_classes import remove_plugin
 from polus.plugins._plugins.classes.plugin_classes import submit_plugin
+=======
+from polus.plugins._plugins.classes.plugin_classes import (
+    ComputePlugin,
+    Plugin,
+    get_plugin,
+    list_plugins,
+    load_config,
+    load_plugin,
+    refresh,
+    remove_all,
+    remove_plugin,
+    submit_plugin,
+)
+>>>>>>> fix: fixed unexported load_config function
 
 __all__ = [
     "Plugin",
@@ -19,4 +34,5 @@ __all__ = [
     "list_plugins",
     "remove_plugin",
     "remove_all",
+    "load_config",
 ]
