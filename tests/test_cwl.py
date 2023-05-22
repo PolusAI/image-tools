dc02e086284130c43fa70a862ff556a438730a37
Camilo Velez <camilov61@gmail.com>
2023-05-22 19:10:35 -0400

    tests: refactored test_cwl

--- tests/test_cwl.py
+++ tests/test_cwl.py
@@ -1,4 +1,5 @@
 # type: ignore
+# pylint: disable=W0621, W0613
 """Tests for CWL utils."""
 from pathlib import Path
 
@@ -8,81 +9,100 @@ import yaml
 import polus.plugins as pp
 from polus.plugins._plugins.classes.plugin_methods import MissingInputValues
 
-OMECONVERTER = Path("./tests/resources/omeconverter030.json")
+TEST_PATH = Path(__file__).parent
+RSRC_PATH = TEST_PATH.joinpath("resources")
+
+OMECONVERTER = RSRC_PATH.joinpath("omeconverter030.json")
 
 
-class TestCWL:
-    """Testing CWL utilities."""
-
-    @classmethod
-    def setup_class(cls):
-        """Configure the class."""
+@pytest.fixture
+def submit_plugin():
+    """Submit OmeConverter plugin."""
+    if "OmeConverter" not in pp.list:
         pp.submit_plugin(OMECONVERTER)
+    else:
+        if "0.3.0" not in [x.version for x in pp.OmeConverter.versions]:
+            pp.submit_plugin(OMECONVERTER)
 
-    def test_save_cwl(self):
-        """Test save_cwl."""
-        plug = pp.get_plugin("OmeConverter", "0.3.0")
-        plug.save_cwl(Path("./tests/resources/omeconverter.cwl"))
-        assert Path("./tests/resources/omeconverter.cwl").exists()
 
-    def test_read_saved_cwl(self):
-        """Test saved cwl."""
-        with open("./tests/resources/omeconverter.cwl", encoding="utf-8") as file:
-            src_cwl = file.read()
-        with open("./tests/resources/target1.cwl", encoding="utf-8") as file:
-            target_cwl = file.read()
-        assert src_cwl == target_cwl
+@pytest.fixture
+def plug(submit_plugin):
+    """Get OmeConverter plugin."""
+    return pp.get_plugin("OmeConverter", "0.3.0")
 
-    def test_save_cwl_io(self):
-        """Test save_cwl IO."""
-        plug = pp.get_plugin("OmeConverter", "0.3.0")
-        plug.inpDir = Path("./tests/resources").absolute()
-        plug.filePattern = "img_r{rrr}_c{ccc}.tif"
-        plug.fileExtension = ".ome.zarr"
-        plug.outDir = Path("./tests/resources").absolute()
-        plug.save_cwl_io(Path("./tests/resources/omeconverter_io.yml"))
 
-    def test_save_cwl_io_not_inp(self):
-        """Test save_cwl IO."""
-        plug = pp.get_plugin("OmeConverter", "0.3.0")
-        with pytest.raises(MissingInputValues):
-            plug.save_cwl_io(Path("./tests/resources/omeconverter_io.yml"))
+@pytest.fixture(scope="session")
+def cwl_io_path(tmp_path_factory):
+    """Temp CWL IO path."""
+    return tmp_path_factory.mktemp("io") / "omeconverter_io.yml"
 
-    def test_save_cwl_io_not_inp2(self):
-        """Test save_cwl IO."""
-        plug = pp.get_plugin("OmeConverter", "0.3.0")
-        plug.inpDir = Path("./tests/resources").absolute()
-        plug.filePattern = "img_r{rrr}_c{ccc}.tif"
-        with pytest.raises(MissingInputValues):
-            plug.save_cwl_io(Path("./tests/resources/omeconverter_io.yml"))
 
-    def test_save_cwl_io_not_yml(self):
-        """Test save_cwl IO."""
-        plug = pp.get_plugin("OmeConverter", "0.3.0")
-        plug.inpDir = Path("./tests/resources").absolute()
-        plug.filePattern = "img_r{rrr}_c{ccc}.tif"
-        plug.fileExtension = ".ome.zarr"
-        plug.outDir = Path("./tests/resources").absolute()
-        with pytest.raises(AssertionError):
-            plug.save_cwl_io(Path("./tests/resources/omeconverter_io.cwl"))
+@pytest.fixture(scope="session")
+def cwl_path(tmp_path_factory):
+    """Temp CWL IO path."""
+    return tmp_path_factory.mktemp("cwl") / "omeconverter.cwl"
 
-    def test_read_cwl_io(self):
-        """Test read_cwl_io."""
-        with open("./tests/resources/omeconverter_io.yml", encoding="utf-8") as file:
-            src_io = yaml.safe_load(file)
-        assert src_io["inpDir"] == {
-            "class": "Directory",
-            "location": str(Path("./tests/resources").absolute()),
-        }
-        assert src_io["outDir"] == {
-            "class": "Directory",
-            "location": str(Path("./tests/resources").absolute()),
-        }
-        assert src_io["filePattern"] == "img_r{rrr}_c{ccc}.tif"
-        assert src_io["fileExtension"] == ".ome.zarr"
 
-    @classmethod
-    def teardown_class(cls):
-        """Teardown."""
-        Path("./tests/resources/omeconverter.cwl").unlink()
-        Path("./tests/resources/omeconverter_io.yml").unlink()
+def test_save_cwl(plug, cwl_path):
+    """Test save_cwl."""
+    plug.save_cwl(cwl_path)
+    assert cwl_path.exists()
+
+
+def test_read_saved_cwl(cwl_path):
+    """Test saved cwl."""
+    with open(cwl_path, encoding="utf-8") as file:
+        src_cwl = file.read()
+    with open(RSRC_PATH.joinpath("target1.cwl"), encoding="utf-8") as file:
+        target_cwl = file.read()
+    assert src_cwl == target_cwl
+
+
+def test_save_cwl_io(plug, cwl_io_path):
+    """Test save_cwl IO."""
+    rs_path = RSRC_PATH.absolute()
+    plug.inpDir = rs_path
+    plug.filePattern = "img_r{rrr}_c{ccc}.tif"
+    plug.fileExtension = ".ome.zarr"
+    plug.outDir = rs_path
+    plug.save_cwl_io(cwl_io_path)
+
+
+def test_save_cwl_io_not_inp(plug, cwl_io_path):
+    """Test save_cwl IO."""
+    with pytest.raises(MissingInputValues):
+        plug.save_cwl_io(cwl_io_path)
+
+
+def test_save_cwl_io_not_inp2(plug, cwl_io_path):
+    """Test save_cwl IO."""
+    plug.inpDir = RSRC_PATH.absolute()
+    plug.filePattern = "img_r{rrr}_c{ccc}.tif"
+    with pytest.raises(MissingInputValues):
+        plug.save_cwl_io(cwl_io_path)
+
+
+def test_save_cwl_io_not_yml(plug, cwl_io_path):
+    """Test save_cwl IO."""
+    plug.inpDir = RSRC_PATH.absolute()
+    plug.filePattern = "img_r{rrr}_c{ccc}.tif"
+    plug.fileExtension = ".ome.zarr"
+    plug.outDir = RSRC_PATH.absolute()
+    with pytest.raises(AssertionError):
+        plug.save_cwl_io(cwl_io_path.with_suffix(".txt"))
+
+
+def test_read_cwl_io(cwl_io_path):
+    """Test read_cwl_io."""
+    with open(cwl_io_path, encoding="utf-8") as file:
+        src_io = yaml.safe_load(file)
+    assert src_io["inpDir"] == {
+        "class": "Directory",
+        "location": str(RSRC_PATH.absolute()),
+    }
+    assert src_io["outDir"] == {
+        "class": "Directory",
+        "location": str(RSRC_PATH.absolute()),
+    }
+    assert src_io["filePattern"] == "img_r{rrr}_c{ccc}.tif"
+    assert src_io["fileExtension"] == ".ome.zarr"
