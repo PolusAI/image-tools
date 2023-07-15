7f6966aec9fa2d8f942eeefdf3f00dded9de6140
Hamdah Abbasi <74803092+hamshkhawar@users.noreply.github.com>
2023-07-14 20:45:17 -0500

    Omeconverter (#417)
    * modified run-plugin.sh and req
    
    * fix test
    
    * fix isort
    
    * isort testing
    
    * removing blank line after doc string
    
    * updating pytest
    
    * modified docker file and added preview
    
    * updating author list and email in toml file
    
    * updating filepattern package
    
    * correcting package name for logger
    
    * addressing comments and adding fixutures
    
    * updating docker container id
    
    * adding batch_convert function and its test
    
    * expose convert_image and batch_convert functions
    
    * adding flake8 in gitignore
    
    * adding flake8 in gitignore
    
    * updating documentation
    
    * modifying yaml file, changed formatting
    
    * Define enums for file_extension
    
    * defining numworkers variable
    
    * fixing docstirng for return
    
    * rewrite pytest, fix typo and define num_threads platform specific
    
    * updating gitignore
    
    * modified pytest
    
    * scalability test
    
    * remove gitignore file
    
    * fix logger level to info
    
    * Handling of channel names if they are missing

---
+++ formats/ome-converter-plugin/src/polus/plugins/formats/ome_converter/__main__.py
@@ -0,0 +1,96 @@
+"""Ome Converter."""
+import json
+import os
+import logging
+import pathlib
+from concurrent.futures import ProcessPoolExecutor, as_completed
+from typing import Any, Optional
+
+import filepattern as fp
+import typer
+from tqdm import tqdm
+
+from polus.plugins.formats.ome_converter.image_converter import (
+    NUM_THREADS,
+    Extension,
+    convert_image,
+)
+
+app = typer.Typer()
+
+# Initialize the logger
+logging.basicConfig(
+    format="%(asctime)s - %(name)-8s - %(levelname)-8s - %(message)s",
+    datefmt="%d-%b-%y %H:%M:%S",
+)
+logger = logging.getLogger("polus.plugins.formats.ome_converter")
+logger.setLevel(os.environ.get("POLUS_LOG", logging.INFO))
+
+
+@app.command()
+def main(
+    inp_dir: pathlib.Path = typer.Option(
+        ...,
+        "--inpDir",
+        help="Input generic data collection to be processed by this plugin",
+    ),
+    pattern: str = typer.Option(
+        ".+", "--filePattern", help="A filepattern defining the images to be converted"
+    ),
+    file_extension: Extension = typer.Option(
+        None, "--fileExtension", help="Type of data conversion"
+    ),
+    out_dir: pathlib.Path = typer.Option(..., "--outDir", help="Output collection"),
+    preview: Optional[bool] = typer.Option(
+        False, "--preview", help="Output a JSON preview of files"
+    ),
+) -> None:
+    """Convert bioformat supported image datatypes conversion to ome.tif or ome.zarr file format."""
+    logger.info(f"inpDir = {inp_dir}")
+    logger.info(f"outDir = {out_dir}")
+    logger.info(f"filePattern = {pattern}")
+    logger.info(f"fileExtension = {file_extension}")
+
+    inp_dir = inp_dir.resolve()
+    out_dir = out_dir.resolve()
+
+    assert inp_dir.exists(), f"{inp_dir} does not exist!! Please check input path again"
+    assert (
+        out_dir.exists()
+    ), f"{out_dir} does not exist!! Please check output path again"
+
+    fps = fp.FilePattern(inp_dir, pattern)
+
+    if preview:
+        with open(pathlib.Path(out_dir, "preview.json"), "w") as jfile:
+            out_json: dict[str, Any] = {
+                "filepattern": pattern,
+                "outDir": [],
+            }
+            for file in fps:
+                out_name = str(file[1][0].name.split(".")[0]) + file_extension
+                out_json["outDir"].append(out_name)
+            json.dump(out_json, jfile, indent=2)
+
+    with ProcessPoolExecutor(max_workers=NUM_THREADS) as executor:
+        threads = []
+        for files in fps():
+            file = files[1][0]
+            threads.append(
+                executor.submit(convert_image, file, file_extension, out_dir)
+            )
+
+        for f in tqdm(
+            as_completed(threads),
+            total=len(threads),
+            mininterval=5,
+            desc=f"converting images to {file_extension}",
+            initial=0,
+            unit_scale=True,
+            colour="cyan",
+        ):
+            f.result()
+
+
+if __name__ == "__main__":
+    app()
