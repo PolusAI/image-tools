094e3580b2c2c218bba5ec5b87a6008cc18c3ea9
hamshkhawar <hamdahshafqat@hotmail.com>
2023-07-28 13:16:17 -0500

    fixed precommit hooks

--- visualization/render-overlay-plugin/run-plugin.sh
+++ visualization/render-overlay-plugin/run-plugin.sh
@@ -2,31 +2,27 @@
 
 version=$(<VERSION)
 datapath=$(readlink --canonicalize data)
+echo ${datapath}
 
-# Inputs
-inp_dir=/data/path_to_images
-seg_dir=/data/path_to_label_images
-int_pattern='p00{z}_x{x+}_y{y+}_wx{t}_wy{p}_c{c}.ome.tif'
-seg_pattern='p00{z}_x{x+}_y{y+}_wx{t}_wy{p}_c1.ome.tif'
-features="BASIC_MORPHOLOGY","ALL_INTENSITY"
-file_extension = ".csv"
-# More details available at https://github.com/PolusAI/nyxus
-neighbor_dist=5.0
-pixel_per_micron=1.0
-out_dir=/data/path_to_output
+# # Inputs
+inp_dir=/data/input
+file_pattern=".*"
+dimension="384"
+geometry_type="Polygon"
+cell_width=2170
+cell_height=2180
+out_dir=/data/output
 
 
-# Show the help options
-# docker run polusai/nyxus-plugin:${version}
+# # Show the help options
+# # docker run polusai/render-overlay-plugin:${version}
 
 docker run --mount type=bind,source=${datapath},target=/data/ \
-            polusai/nyxus-plugin:${version} \
+            polusai/render-overlay-plugin:${version} \
             --inpDir ${inp_dir} \
-            --segDir ${seg_dir} \
-            --outDir ${out_dir} \
-            --intPattern ${int_pattern} \
-            --segPattern ${seg_pattern} \
-            --features ${features} \
-            --fileExtension ${file_extension} \
-            --neighborDist ${neighbor_dist} \
-            --pixelPerMicron ${pixel_per_micron} \
+            --filePattern ${file_pattern} \
+            --dimension ${dimension} \
+            --geometryType ${geometry_type} \
+            --cellWidth ${cell_width} \
+            --cellHeight ${cell_height} \
+            --outDir ${out_dir}
