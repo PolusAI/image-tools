e3edfed64748252778817596d164545c5353f2d4
hamshkhawar <hamdahshafqat@hotmail.com>
2023-07-28 13:16:17 -0500

    fixed precommit hooks

--- visualization/render-overlay-plugin/src/polus/plugins/visualization/render_overlay/__main__.py
+++ visualization/render-overlay-plugin/src/polus/plugins/visualization/render_overlay/__main__.py
@@ -1,16 +1,17 @@
-"""File Renaming."""
-import os
+"""Render Overlay."""
 import logging
+import os
 import pathlib
-import filepattern as fp
-from typing import Optional
-import typer
-from multiprocessing import cpu_count
-from concurrent.futures import ThreadPoolExecutor, as_completed
-from tqdm import tqdm
 import shutil
+from concurrent.futures import ThreadPoolExecutor
+from concurrent.futures import as_completed
+from multiprocessing import cpu_count
+from typing import Optional
 
+import filepattern as fp
+import typer
 from polus.plugins.visualization.render_overlay import mircojson_overlay as mo
+from tqdm import tqdm
 
 app = typer.Typer()
 
@@ -26,32 +27,36 @@ num_workers = max(cpu_count() // 2, 2)
 
 
 @app.command()
-def main(
+def main(  # noqa: PLR0913
     inp_dir: pathlib.Path = typer.Option(
-        ..., "--inpDir", help="Input image collections"
+        ...,
+        "--inpDir",
+        help="Path to input directory containing tabular data",
     ),
     file_pattern: str = typer.Option(
-        ".+", "--filePattern", help="Filename pattern used to separate data"
+        ".+",
+        "--filePattern",
+        help="Filename pattern used to separate data",
     ),
     dimensions: Optional[mo.Dimensions] = typer.Option(
         mo.Dimensions.DEFAULT,
         "--dimensions",
         help="Plate dimension (Columns, Rows)",
     ),
-    type: Optional[str] = typer.Option(
+    geometry_type: Optional[str] = typer.Option(
         "Polygon",
-        "--type",
+        "--geometryType",
         help="Type of Geometry",
     ),
     cell_width: int = typer.Option(
         ...,
         "--cellWidth",
-        help="Number of pixels in x-dimension",
+        help="Pixel distance between adjacent cells/wells in x-dimension",
     ),
     cell_height: int = typer.Option(
         ...,
         "--cellHeight",
-        help="Number of pixels in y-dimension",
+        help="Pixel distance in y-dimension",
     ),
     out_dir: pathlib.Path = typer.Option(
         ...,
@@ -59,13 +64,16 @@ def main(
         help="Path to output directory",
     ),
     preview: Optional[bool] = typer.Option(
-        False, "--preview", help="Output a JSON preview of files"
+        False,
+        "--preview",
+        help="Output a JSON preview of files",
     ),
 ) -> None:
+    """Apply Render Overlay to input tabular data to create microjson overlays."""
     logger.info(f"inpDir = {inp_dir}")
     logger.info(f"filePattern = {file_pattern}")
     logger.info(f"dimensions = {dimensions}")
-    logger.info(f"type = {type}")
+    logger.info(f"geometryType = {geometry_type}")
     logger.info(f"cellWidth = {cell_width}")
     logger.info(f"cellHeight = {cell_height}")
     logger.info(f"outDir = {out_dir}")
@@ -73,33 +81,36 @@ def main(
     inp_dir = inp_dir.resolve()
     out_dir = out_dir.resolve()
 
-    width, height = dimensions.get_value()
+    if dimensions is not None:
+        width, height = dimensions.get_value()
 
     files = [file[1][0] for file in fp.FilePattern(inp_dir, file_pattern)]
 
     with ThreadPoolExecutor(max_workers=num_workers) as executor:
         threads = []
 
-        for file in tqdm(files, desc=f"Creating overlays", total=len(files)):
+        for file in tqdm(files, desc="Creating overlays", total=len(files)):
             cells = mo.GridCell(width=width, height=height, cell_width=cell_width)
             if type == "Polygon":
                 poly = mo.PolygonSpec(
-                    positions=cells.convert_data, cell_height=cell_height
+                    positions=cells.convert_data,
+                    cell_height=cell_height,
                 )
             else:
                 poly = mo.PointSpec(
-                    positions=cells.convert_data, cell_height=cell_height
+                    positions=cells.convert_data,
+                    cell_height=cell_height,
                 )
             microjson = mo.RenderOverlayModel(
                 file_path=file,
                 coordinates=poly.get_coordinates,
-                type=type,
+                type=geometry_type,
                 out_dir=out_dir,
             )
             threads.append(executor.submit(microjson.microjson_overlay))
         for f in tqdm(
             as_completed(threads),
-            desc=f"Creating microjson overlays",
+            desc="Creating microjson overlays",
             total=len(threads),
         ):
             f.result()
