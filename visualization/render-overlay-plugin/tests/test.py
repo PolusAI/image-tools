e567211a05051d3c8a93ed4ea69c7ddffdb35fdc
hamshkhawar <hamdahshafqat@hotmail.com>
2023-08-02 13:01:54 -0500

    implemented random generator

--- visualization/render-overlay-plugin/tests/test.py
+++ visualization/render-overlay-plugin/tests/test.py
@@ -1,7 +1,6 @@
 """Test for Render Overlay."""
 import json
 import pathlib
-import random
 import shutil
 import string
 import tempfile
@@ -29,48 +28,56 @@ def input_directory() -> pathlib.Path:
     return pathlib.Path(tempfile.mkdtemp(dir=pathlib.Path.cwd()))
 
 
-EXT = [".csv", ".arrow", ".feather"]
-
-
-@pytest.fixture(params=EXT)
-def file_extension(request: pytest.FixtureRequest) -> str:
+@pytest.fixture(
+    params=[
+        (24, 16, 2170, 1080, "Polygon", 384, ".csv"),
+        (12, 8, 1080, 3240, "Polygon", 96, ".arrow"),
+        (6, 4, 100, 3240, "Point", 24, ".feather"),
+        (3, 2, 50, 1080, "Point", 6, ".csv"),
+    ],
+)
+def get_params(request: pytest.FixtureRequest) -> tuple[int, int, int, int, int, str]:
     """To get the parameter of the fixture."""
     return request.param
 
 
 @pytest.fixture()
 def generate_synthetic_data(
-    file_extension: pytest.FixtureRequest,
-    input_directory: pytest.FixtureRequest,
+    input_directory: pathlib.Path,
+    get_params: tuple[int, int, int, int, str, int, str],
 ) -> pathlib.Path:
     """Generate tabular data."""
-    size = 384
+    width, height, _, _, _, dimension, file_extension = get_params
+
+    rng = np.random.default_rng(42)
     diction_1 = {
-        "Plate": np.repeat("preZ", size).tolist(),
+        "Plate": np.repeat("preZ", dimension).tolist(),
         "Well": [
-            f"{s}{num}" for s in string.ascii_letters.upper()[:16] for num in range(24)
+            f"{s}{num}"
+            for s in string.ascii_letters.upper()[:height]
+            for num in range(width)
         ],
         "Characteristics [Organism 2]": np.repeat(
             "Herpes simplex virus type 1",
-            size,
+            dimension,
         ).tolist(),
-        "Characteristics [Cell Line]": np.repeat("A549", size).tolist(),
+        "Characteristics [Cell Line]": np.repeat("A549", dimension).tolist(),
         "Compound Name": [
-            random.choice(["DMSO", "Ganciclovir"]) for i in range(size)  # noqa: S311
+            rng.choice(["DMSO", "Ganciclovir"]) for i in range(dimension)
         ],
         "Control Type": [
-            random.choice(["negative control", "positive control"])  # noqa: S311
-            for i in range(size)
+            rng.choice(["negative control", "positive control"])
+            for i in range(dimension)
         ],
-        "numberOfNuclei": np.random.randint(  # noqa:  NPY002
+        "numberOfNuclei": rng.integers(
             low=2500,
             high=100000,
-            size=size,
+            size=dimension,
         ),
-        "maxVirusIntensity": np.random.randint(  # noqa:  NPY002
+        "maxVirusIntensity": rng.integers(
             low=500,
             high=30000,
-            size=size,
+            size=dimension,
         ),
     }
 
@@ -97,22 +104,11 @@ def test_convert_vaex_dataframe(generate_synthetic_data: pathlib.Path) -> None:
     shutil.rmtree(generate_synthetic_data.parent)
 
 
-@pytest.fixture(
-    params=[
-        (24, 16, 2170, 1080, "Polygon"),
-        (12, 8, 1080, 3240, "Polygon"),
-        (6, 4, 100, 3240, "Point"),
-        (3, 3, 50, 1080, "Point"),
-    ],
-)
-def get_params(request: pytest.FixtureRequest) -> tuple[int, int, int, int, str]:
-    """To get the parameter of the fixture."""
-    return request.param
-
-
-def test_generate_gridcell(get_params: tuple[int, int, int, int, str]) -> None:
+def test_generate_gridcell(
+    get_params: tuple[int, int, int, int, str, int, str],
+) -> None:
     """Test grid positons of microplate."""
-    width, height, cell_width, _, _ = get_params
+    width, height, cell_width, _, _, _, _ = get_params
 
     cells = mo.GridCell(width=width, height=height, cell_width=cell_width)
     gridcells = cells.convert_data
@@ -120,10 +116,10 @@ def test_generate_gridcell(get_params: tuple[int, int, int, int, str]) -> None:
 
 
 def test_generate_polygon_coordinates(
-    get_params: tuple[int, int, int, int, str],
+    get_params: tuple[int, int, int, int, str, int, str],
 ) -> None:
     """Test generating polygon coordinates of a microplate."""
-    width, height, cell_width, cell_height, _ = get_params
+    width, height, cell_width, cell_height, _, _, _ = get_params
     cells = mo.GridCell(width=width, height=height, cell_width=cell_width)
     poly = mo.PolygonSpec(
         positions=cells.convert_data,
@@ -134,10 +130,10 @@ def test_generate_polygon_coordinates(
 
 
 def test_generate_rectangular_polygon_centroids(
-    get_params: tuple[int, int, int, int, str],
+    get_params: tuple[int, int, int, int, str, int, str],
 ) -> None:
     """Test generating centroid rectangular coordinates of a microplate."""
-    width, height, cell_width, cell_height, _ = get_params
+    width, height, cell_width, cell_height, _, _, _ = get_params
     cells = mo.GridCell(width=width, height=height, cell_width=cell_width)
     poly = mo.PointSpec(
         positions=cells.convert_data,
@@ -150,10 +146,10 @@ def test_generate_rectangular_polygon_centroids(
 def test_render_overlay_model(
     generate_synthetic_data: pathlib.Path,
     output_directory: pathlib.Path,
-    get_params: tuple[int, int, int, int, str],
+    get_params: tuple[int, int, int, int, str, int, str],
 ) -> None:
     """Test render overlay model."""
-    width, height, cell_width, cell_height, geometry_type = get_params
+    width, height, cell_width, cell_height, geometry_type, _, _ = get_params
     cells = mo.GridCell(width=width, height=height, cell_width=cell_width)
 
     if geometry_type == "Polygon":
@@ -183,9 +179,14 @@ def test_render_overlay_model(
 
 
 @pytest.fixture(
-    params=[("384", "Polygon"), ("96", "Polygon"), ("24", "Point"), ("6", "Point")],
+    params=[
+        (2170, 1080, "Polygon", "384"),
+        (1080, 3240, "Polygon", "96"),
+        (100, 3240, "Point", "24"),
+        (50, 1080, "Point", "6"),
+    ],
 )
-def get_cli_params(request: pytest.FixtureRequest) -> tuple[str, str]:
+def get_cli(request: pytest.FixtureRequest) -> tuple[int, int, str, str]:
     """To get the parameter of the fixture."""
     return request.param
 
@@ -193,12 +194,11 @@ def get_cli_params(request: pytest.FixtureRequest) -> tuple[str, str]:
 def test_cli(
     generate_synthetic_data: pathlib.Path,
     output_directory: pathlib.Path,
-    get_cli_params: tuple[str, str],
+    get_cli: tuple[int, int, str, str],
 ) -> None:
     """Test Cli."""
     inp_dir = generate_synthetic_data.parent
-
-    dimen, geo_shape = get_cli_params
+    cell_width, cell_height, geometry_type, dimension = get_cli
 
     result = runner.invoke(
         app,
@@ -208,13 +208,13 @@ def test_cli(
             "--filePattern",
             ".+",
             "--dimensions",
-            dimen,
+            dimension,
             "--geometryType",
-            geo_shape,
+            geometry_type,
             "--cellWidth",
-            2170,
+            cell_width,
             "--cellHeight",
-            1080,
+            cell_height,
             "--outDir",
             pathlib.Path(output_directory),
         ],
