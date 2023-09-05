6810a8b0a7cf7b95aeb71ea3d33ad068b257f354
agerardin <antoine.gerardin@gmail.com>
2023-09-05 11:36:48 -0400

    fix: test_zarr_pyramid

--- visualization/precompute-slide-plugin/tests/test_zarr_pyramid.py
+++ visualization/precompute-slide-plugin/tests/test_zarr_pyramid.py
@@ -7,34 +7,38 @@ import random
 from polus.plugins.visualization.precompute_slide.precompute_slide import precompute_slide
 import os
 import zarr
+from tests.fixtures import plugin_dirs, get_temp_file
 
-def get_temp_file(path: Path, suffix: str):
-    """Create path to a temp file."""
-    temp_name = next(tempfile._get_candidate_names())
-    return path / (temp_name + suffix)
+# def get_temp_file(path: Path, suffix: str):
+#     """Create path to a temp file."""
+#     temp_name = next(tempfile._get_candidate_names())
+#     return path / (temp_name + suffix)
 
 
-@fixture
-def plugin_dirs(tmp_path):
-    """Create temporary directories"""
-    input_dir = tmp_path / "input_dir"
-    output_dir = tmp_path / "output_dir"
-    input_dir.mkdir()
-    output_dir.mkdir()
-    return (input_dir, output_dir)
+# @fixture
+# def plugin_dirs(tmp_path):
+#     """Create temporary directories"""
+#     input_dir = tmp_path / "input_dir"
+#     output_dir = tmp_path / "output_dir"
+#     input_dir.mkdir()
+#     output_dir.mkdir()
+#     return (input_dir, output_dir)
 
 @fixture
 def image_file(plugin_dirs):
+    """
+    Generate a simple base image of a centered white square 
+    over a black background that can be easily check visually.
+    We will build the pyramid from it.
+    """
 
-    input_dir, output_dir = plugin_dirs
+    input_dir, _ = plugin_dirs
 
-    # generate the image data
-    tile_size = 1024
+    # generate the base image data
     image_height = 2048
     image_width = 2048
     imageShape = (image_width, image_height, 1, 1, 1)
     data = np.zeros(imageShape, dtype=np.uint8)
-
     FILL_VALUE = 127
     center_x = image_width // 2
     center_y = image_height // 2
@@ -43,7 +47,7 @@ def image_file(plugin_dirs):
         center_y - center_y // 2 : center_y + center_y //2
          ] = FILL_VALUE
 
-    # generate the ground truth image
+    # write our base image
     suffix = ".ome.tiff"
     image_file = get_temp_file(input_dir, suffix)
     with BioWriter(image_file) as writer:
@@ -55,30 +59,58 @@ def image_file(plugin_dirs):
 
 
 def test_zarr_pyramid(plugin_dirs, image_file):
-
+    """
+    Test the creation of a zarr pyramid.
+    The tests are mostly checking that the output zarr is a valid zarr pyramid.
+    TODO at this moment, I did not find an authoritative source for specifying 
+    zarr pyramids.
+    """
+    
     input_dir, output_dir = plugin_dirs
-
-    # check assembled image against ground truth
-    with BioReader(image_file) as image:
-        print("image size : ", image.shape)
 
     precompute_slide(input_dir, "Zarr", "image", ".*", output_dir)
 
-    output = output_dir / os.listdir(output_dir)[0]
-    output = output / os.listdir(output)[0]
-    output = zarr.open_group(output, mode="r+")
-    print(output.tree())
+    zarr_dir : Path = output_dir / os.listdir(output_dir)[0]
 
-    levels = list(output['0'].arrays())
+    # check the top directory match the image name
+    image_stem = image_file.name
+    for suffix in image_file.suffixes:
+        image_stem = image_stem.replace(suffix,"")
+    assert(image_stem == zarr_dir.name)
 
-    print("output :", levels)
+    # check we have a first zarr group
+    zarr_top_level_group = None
+    for f in zarr_dir.iterdir():
+        if f.is_dir():
+            assert f.name == 'data.zarr'
+            zarr_top_level_group = f
+        else :
+            assert f.name == "METADATA.ome.xml"
+    
+    # check we have a zarr subgroup
+    zarr_second_level_group = None
+    for f in zarr_top_level_group.iterdir():
+        if f.is_dir():
+            assert f.name == '0'
+            zarr_second_level_group = f
+        else :
+            assert f.name == ".zgroup"
+    
+    # check we have several pyramid levels
+    top_level = zarr.open_group(zarr_top_level_group, mode="r+")
+    second_level = zarr.open_group(zarr_second_level_group, mode="r+")
+    # print(top_level.tree())
+    
 
-    # TODO Check what is the cutoff
+    # TODO Check what is the cutoff. Is it always 12?
+    levels = list(second_level.arrays())
     assert(len(list(levels)) == 12)
+    # print(levels)
 
     # TODO This seems to be because bfio gives you a view
     # the data with shape (X,Y,Z,C,T)
     # despite the data being stored as such : (T,C,Z,Y,X)
-    reversed_shape = (list(reversed(image.shape)))
-
-    assert(levels[0][1].shape == reversed_shape)
+    with BioReader(image_file) as image:
+        original_image_shape = list(reversed(image.shape))
+        level0_shape = list(levels[0][1].shape)
+        assert( level0_shape == original_image_shape)
