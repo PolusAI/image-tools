afaff159bf2b7c5757610ada0a8a6f689d41e294
Hamdah Shafqat Abbasi <74803092+hamshkhawar@users.noreply.github.com>
2023-09-22 12:28:14 -0500

    Update/pixel-segmentation-comparison-plugin (#431)
    * updating gitignore
    
    * Pixelwise Evaluation Plugin
    
    * updating plugin, implemented support for other fileformats
    
    * fixing pluin name
    
    * modify docker files and added changelog
    
    * ignoring flake8
    
    * ignoring flake8
    
    ---------
    
    Co-authored-by: vishakha6 <vishakha.goyal6@gmail.com>
    Co-authored-by: Nicholas-Schaub <nick.schaub@axleinfo.com>

---
+++ features/pixel-segmentation-comparison-plugin/run-plugin.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+version=$(<VERSION)
+datapath=$(readlink --canonicalize data)
+
+# Inputs
+gtDir=/data/groundtruth
+predDir=/data/predicted_images
+inputClasses=1
+filePattern=".+"
+fileExtension=".arrow"
+
+# Output paths
+outDir=/data/output
+
+# Show the help options
+# docker run polusai/pixel-segmentation-comparison-plugin:${version}
+
+# Run the plugin
+docker run --mount type=bind,source=${datapath},target=/data/ \
+            polusai/pixel-segmentation-comparison-plugin:${version} \
+            --gtDir ${gtDir} \
+            --predDir ${predDir} \
+            --inputClasses ${inputClasses} \
+            --filePattern ${filePattern} \
+            --individualStats \
+            --totalStats \
+            --fileExtension ${fileExtension} \
+            --outDir ${outDir}
