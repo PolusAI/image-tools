1dd42a1e02a160d1adde6740a6ad314c03b8f854
Najib <32004868+nishaq503@users.noreply.github.com>
2023-10-05 09:38:04 -0400

    Plugin update: roi-relabel (#418)
    * feat: updated roi-relabel-plugin
    
    * build: bump version to 0.2.2 for roi-relabel plugin

--- transforms/images/polus-roi-relabel-plugin/roi_relabel/utils/__init__.py
+++
@@ -1,3 +0,0 @@
-from . import constants as constants
-from . import helpers as helpers
-from . import types as types
--- transforms/images/roi-relabel-plugin/run-plugin.sh
+++ transforms/images/roi-relabel-plugin/run-plugin.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-version=$(grep -m 1 version pyproject.toml | tr -s ' ' | tr -d '"' | tr -d "'" | cut -d' ' -f3)
+version=$(<VERSION)
+
 data_path=$(readlink --canonicalize ./data)
 
 # Change to .ome.zarr to save output images as zarr files.
-POLUS_EXT=".ome.tif"
+POLUS_IMG_EXT=".ome.tif"
+POLUS_TAB_EXT=".csv"
 
 # Must be one of ERROR, CRITICAL, WARNING, INFO, DEBUG
-POLUS_LOG=INFO
+POLUS_LOG="INFO"
 
 # Inputs
 inpDir=/data/input
@@ -16,11 +18,15 @@ method="randomize"
 # Outputs
 outDir=/data/output
 
+# Show help
+docker run polusai/roi-relabel-plugin:"${version}"
+
 docker run --mount type=bind,source="${data_path}",target=/data \
             --user "$(id -u)":"$(id -g)" \
-            --env POLUS_EXT=${POLUS_EXT} \
-            --env POLUS_LOG=${POLUS_LOG} \
+            --env POLUS_IMG_EXT="${POLUS_IMG_EXT}" \
+            --env POLUS_TAB_EXT="${POLUS_TAB_EXT}" \
+            --env POLUS_LOG="${POLUS_LOG}" \
             polusai/roi-relabel-plugin:"${version}" \
-            --inpDir ${inpDir} \
-            --method ${method} \
-            --outDir ${outDir}
+            --inpDir "${inpDir}" \
+            --method "${method}" \
+            --outDir "${outDir}"
