90f8335eaa723b8af0960e336fd3d190ad6e7d85
Camilo Velez <camilov61@gmail.com>
2023-10-26 15:36:17 -0400

    fix: fixed last rebase

--- src/polus/plugins/_plugins/classes/__init__.py
+++ src/polus/plugins/_plugins/classes/__init__.py
@@ -1,33 +1,17 @@
 """Plugin classes and functions."""
-<<<<<<< rm1
 from polus.plugins._plugins.classes.plugin_classes import ComputePlugin
 from polus.plugins._plugins.classes.plugin_classes import Plugin
-from polus.plugins._plugins.classes.plugin_classes import _load_plugin
 from polus.plugins._plugins.classes.plugin_classes import get_plugin
 from polus.plugins._plugins.classes.plugin_classes import list_plugins
+from polus.plugins._plugins.classes.plugin_classes import load_config
 from polus.plugins._plugins.classes.plugin_classes import refresh
 from polus.plugins._plugins.classes.plugin_classes import remove_all
 from polus.plugins._plugins.classes.plugin_classes import remove_plugin
 from polus.plugins._plugins.classes.plugin_classes import submit_plugin
-=======
-from polus.plugins._plugins.classes.plugin_classes import (
-    ComputePlugin,
-    Plugin,
-    get_plugin,
-    list_plugins,
-    load_config,
-    load_plugin,
-    refresh,
-    remove_all,
-    remove_plugin,
-    submit_plugin,
-)
->>>>>>> fix: fixed unexported load_config function
 
 __all__ = [
     "Plugin",
     "ComputePlugin",
-    "_load_plugin",
     "submit_plugin",
     "get_plugin",
     "refresh",
