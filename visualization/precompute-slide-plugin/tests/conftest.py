68c6f49770528898d73c8a0435c76d2e8060dc0a
agerardin <antoine.gerardin@gmail.com>
2023-12-22 08:36:06 -0500

    chore: minor changes to pass precommit hooks.

--- visualization/precompute-slide-plugin/tests/conftest.py
+++ visualization/precompute-slide-plugin/tests/conftest.py
@@ -1,12 +1,6 @@
+"""" Configure pytest."""
 import pytest
-import tempfile
-import pathlib
-import shutil
-import itertools
 
-from . import helpers
-from polus.plugins.visualization.precompute_slide.utils import ImageType
-from polus.plugins.visualization.precompute_slide.utils import PyramidType
 
 def pytest_addoption(parser: pytest.Parser) -> None:
     """Add options to pytest."""
