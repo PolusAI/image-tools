3e78e42cd11da6ddd0973ad02a2b2a5dce1e3985
hamshkhawar <hamdahshafqat@hotmail.com>
2024-01-08 14:32:43 -0600

    fixed output argument in shell script

--- utils/rxiv-download/run-plugin.sh
+++ utils/rxiv-download/run-plugin.sh
@@ -5,15 +5,16 @@ datapath=$(readlink --canonicalize data)
 echo ${datapath}
 
 # Inputs
-path=${datapath}/input
+out_dir=${datapath}/out
 rxiv="arXiv"
 start="2023-12-16"
+
 
 # #Show the help options
 # #docker run polusai/rxiv-download-plugin:${version}
 
 docker run -v ${datapath}:${datapath} \
             polusai/rxiv-download-plugin:${version} \
-            --path ${path} \
             --rxiv ${rxiv} \
-            --start ${start}
+            --start ${start} \
+            --outDir ${out_dir}
