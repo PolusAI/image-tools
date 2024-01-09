0478d6049537fcbf4d3e6f344b01dc0ef3a498a3
hamshkhawar <hamdahshafqat@hotmail.com>
2024-01-09 10:16:51 -0600

    fix merge conflicts

---
+++ formats/tabular-converter-plugin/src/polus/plugins/formats/tabular_converter/__init__.py
@@ -0,0 +1,9 @@
+"""Tabular Converter."""
+__version__ = "0.1.0"
+
+from polus.plugins.formats.tabular_converter.tabular_converter import (  # noqa
+    Convert_tabular as Convert_tabular,
+)
+from polus.plugins.formats.tabular_converter.tabular_converter import (  # noqa
+    Extension as Extension,
+)
