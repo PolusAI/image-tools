816c05e47261e3213e69e78def2ed1a8d69b9a54
Hamdah Shafqat Abbasi <74803092+hamshkhawar@users.noreply.github.com>
2024-01-10 09:15:55 -0600

    Updating Mesmer Inference Plugin (#433)
    * Mesmer Inference Plugin
    
    * sync with upstream master
    
    * fix merge conflicts
    
    ---------
    
    Co-authored-by: vishakha6 <vishakha.goyal6@gmail.com>

---
+++ segmentation/mesmer-inference-plugin/run-plugin.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+version=$(<VERSION)
+datapath=$(readlink --canonicalize data)
+
+# Inputs
+inpDir=/data/intensity
+tileSize=512
+modelPath="path/to/model"
+filePatternTest="y{y+}_r{r+}_c0.ome.tif"
+filePatternWholeCell = "y{y+}_r{r+}_c1.ome.tif"
+model="mesmerNuclear"
+fileExtension=".ome.tif"
+
+# Output paths
+outDir=/data/output
+
+# Show the help options
+docker run polusai/mesmer-inference-plugin:${version}
+
+# Run the plugin
+docker run --mount type=bind,source=${datapath},target=/data/ \
+            polusai/mesmer-inference-plugin:${version} \
+            --inpDir ${inpDir} \
+            --tileSize ${tileSize} \
+            --modelPath ${modelPath} \
+            --filePatternTest ${filePatternTest} \
+            --filePatternWholeCell ${filePatternWholeCell} \
+            --model ${model} \
+            --fileExtension ${fileExtension} \
+            --outDir ${outDir}
