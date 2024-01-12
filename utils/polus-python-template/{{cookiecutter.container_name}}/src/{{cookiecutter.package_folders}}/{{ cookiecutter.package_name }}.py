60905d22ff49e4125e3411a9bc128e6e439b08e6
agerardin <antoine.gerardin@gmail.com>
2024-01-12 09:46:27 -0500

    --amend

--- utils/polus-python-template/{{cookiecutter.container_name}}/src/{{cookiecutter.package_folders}}/{{ cookiecutter.package_name }}.py
+++ utils/polus-python-template/{{cookiecutter.container_name}}/src/{{cookiecutter.package_folders}}/{{ cookiecutter.package_name }}.py
@@ -4,4 +4,13 @@ from pathlib import Path
 
 
 def {{cookiecutter.package_name}}(inp_dir: Path, filepattern: str, out_dir: Path):
-    """Do something awesome."""
+    """{{cookiecutter.plugin_name}}.
+
+    Args:
+        inp_dir: input directory to process
+        filepattern: filepattern to filter inputs
+        out_dir: output directory
+    Returns:
+        None
+    """
+    pass
