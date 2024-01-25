d384b72b8c1d48eec67bc80cb26c6f7fa1f95cc6
hamshkhawar <hamdahshafqat@hotmail.com>
2024-01-25 11:56:25 -0600

    Added two more tests

--- clustering/feature-subsetting-plugin/tests/test_feature_subsetting.py
+++ clustering/feature-subsetting-plugin/tests/test_feature_subsetting.py
@@ -39,3 +39,33 @@ def test_feature_subset(
     shutil.rmtree(inp_dir)
     shutil.rmtree(out_dir)
     shutil.rmtree(tabular_dir)
+
+
+def test_filter_planes() -> None:
+    """Test filter planes."""
+    feature_dict = {
+        1: 1236.597914951989,
+        2: 1153.754875685871,
+        3: 1537.3429175240055,
+        4: 1626.0415809327849,
+    }
+
+    percentile = 0.1
+    remove_direction = "Below"
+    fn = fs.filter_planes(
+        feature_dict=feature_dict,
+        remove_direction=remove_direction,
+        percentile=percentile,
+    )
+
+    assert type(fn) == set
+
+
+def test_make_uniform() -> None:
+    """Test each section contain same number of images."""
+    planes_dict = {1: [3, 4]}
+    uniques = [1, 2, 3, 4]
+    padding = 0
+    fn = fs.make_uniform(planes_dict=planes_dict, uniques=uniques, padding=padding)
+
+    assert len(fn) != 0
