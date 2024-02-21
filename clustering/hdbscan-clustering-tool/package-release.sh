5ac8a1aa0a2257e3e0bbb4b91c0c8a023f2a6bfc
hamshkhawar <hamdahshafqat@hotmail.com>
2024-02-21 12:59:54 -0600

    fixed docker files

--- clustering/hdbscan-clustering-tool/package-release.sh
+++ clustering/hdbscan-clustering-tool/package-release.sh
@@ -10,7 +10,7 @@ bump2version --config-file bumpversion.cfg --new-version ${version} --allow-dirt
 ./build-docker.sh
 
 # Push to dockerhub
-docker push polusai/hdbscan-clustering-plugin:${version}
+docker push polusai/hdbscan-clustering-tool:${version}
 
 # Run pytests
 python -m pytest -s tests
