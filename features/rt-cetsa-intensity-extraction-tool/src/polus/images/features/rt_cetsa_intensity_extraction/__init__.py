ef82467cfbaac80970049de980cf189d2b6a62bf
agerardin <antoine.gerardin@gmail.com>
2024-05-13 07:02:35 -0400

    fix: rt_cetsa intensity extraction

--- features/rt-cetsa-intensity-extraction-tool/src/polus/images/features/rt_cetsa_intensity_extraction/__init__.py
+++ features/rt-cetsa-intensity-extraction-tool/src/polus/images/features/rt_cetsa_intensity_extraction/__init__.py
@@ -1,8 +1,8 @@
 """RT_CETSA Intensity Extraction Tool."""
 
-__version__ = "0.1.0"
-
 import itertools
+import logging
+import os
 import pathlib
 import string
 from enum import Enum
@@ -11,11 +11,16 @@ import bfio
 import numpy
 import numpy as np
 import pandas
-from skimage.draw import disk
-from skimage.transform import rotate
+
+logger = logging.getLogger(__file__)
+logger.setLevel(os.environ.get("POLUS_LOG", logging.INFO))
 
 ADD_TEMP = True
 TEMPERATURE_RANGE = [37, 90]
+
+
+class IntensityExtractionError(Exception):
+    pass
 
 
 class PlateSize(Enum):
@@ -33,24 +38,88 @@ PLATE_DIMS = {
     PlateSize.SIZE_12: (3, 4),
     PlateSize.SIZE_24: (4, 6),
     PlateSize.SIZE_48: (6, 8),
-    PlateSize.SIZE_96: (9, 12),
+    PlateSize.SIZE_96: (8, 12),
     PlateSize.SIZE_384: (16, 24),
     PlateSize.SIZE_1536: (32, 48),
 }
 
 
-def extract_intensities(
+def extract_signal(
+    img_paths: list[pathlib.Path],
+    mask_path: pathlib.Path,
+) -> pandas.DataFrame:
+    """Build a DataFrame with well intensity measurements for each temperature.
+
+    Args:
+        img_paths: List of image paths.
+        mask_path: path to the wells mask.
+
+    Returns:
+        Pandas DataFrame.
+    """
+    if not ADD_TEMP:
+        raise NotImplementedError
+
+    num_images = len(img_paths)
+
+    if num_images < 2:
+        raise ValueError(
+            "provide at least 2 images on the temperature interval "
+            + f"({TEMPERATURE_RANGE[0]}-{TEMPERATURE_RANGE[1]})",
+        )
+
+    measures: list[tuple[float, list[int]]] = []
+    for index, image_path in enumerate(img_paths):
+        temp = TEMPERATURE_RANGE[0] + index / (num_images - 1) * (
+            TEMPERATURE_RANGE[1] - TEMPERATURE_RANGE[0]
+        )
+        try:
+            row = (temp, extract_wells_intensity(image_path, mask_path))
+            measures.append(row)
+        except Exception as e:  # noqa
+            raise IntensityExtractionError(
+                f"could not process image extracted intensity for image : {index+1}/{num_images}",
+            ) from e
+        logger.info(f"extracted intensity for image : {index+1}/{num_images}")
+
+    # build header
+    # check the first plate for number of wells
+    nb_wells = len(measures[0][1])
+    plate_size = PlateSize(nb_wells)
+    plate_row = range(PLATE_DIMS[plate_size][0])
+    plate_col = range(PLATE_DIMS[plate_size][1])
+    plate_coords = itertools.product(plate_row, plate_col)
+
+    header = ["Temperature"] + [
+        alphanumeric_row(row, col, plate_size) for row, col in plate_coords
+    ]
+
+    # build dataframe
+    # roundup temperature
+    rows = [[round(measure[0], 1), *measure[1]] for measure in measures]
+    df = pandas.DataFrame(rows, columns=header)
+
+    # Set the temperature as the index
+    df.set_index("Temperature", inplace=True)
+
+    # Sort the rows by temperature
+    df.sort_index(inplace=True)
+
+    return df
+
+
+def extract_wells_intensity(
     image_path: pathlib.Path,
     mask_path: pathlib.Path,
 ) -> list[int]:
     """Extract well intensities from RT_CETSA image and mask.
 
     Args:
-        image_path: Path to the RT_CETSA image.
-        mask_path: Path to the mask image.
+        image_path: Path to the RT_CETSA well plate image.
+        mask_path: Path to the corresponding mask image.
 
     Returns:
-        Pandas DataFrame with well intensities.
+        mean intensity for each wells.
     """
     with bfio.BioReader(image_path) as reader:
         image = reader[:]
@@ -59,102 +128,48 @@ def extract_intensities(
 
     max_mask_index = numpy.max(mask)
     intensities = []
-    for i in range(1, max_mask_index + 1):
-        current_mask = mask == i
-        image[current_mask]
 
-        # find a square bounding box around the mask
+    for mask_label in range(1, max_mask_index + 1):
+        # retrieve a bounding box around each well.
+        # NOTE this was originally much faster when relying on well positions.
+        current_mask = mask == mask_label
+        image[current_mask]
         bbox = numpy.argwhere(current_mask)
         bbox_x_min = numpy.min(bbox[0])
         bbox_x_max = numpy.max(bbox[0])
         bbox_y_min = numpy.min(bbox[1])
         bbox_y_max = numpy.max(bbox[1])
 
+        # compute corrected intensity
         patch = image[bbox_y_min:bbox_y_max, bbox_x_min:bbox_x_max]
         background = patch.ravel()
         background.sort()
+        # Subtract lowest pixel values from average pixel values
         corrected_mean_intensity = int(
             np.mean(patch) - np.mean(background[: int(0.05 * background.size)]),
         )
 
-        # Subtract lowest pixel values from average pixel values
         intensities.append(corrected_mean_intensity)
 
     return intensities
 
 
-def index_to_battleship(x: int, y: int, size: PlateSize) -> str:
-    """Get the battleship notation of a well index.
-
-    Args:
-        x: x-position of the well centerpoint
-        y: y-position of the well centerpoint
-        size: size of the plate
-
-    Returns:
-        str: The string representation of the well index (i.e. A1)
+def alphanumeric_row(row: int, col: int, size: PlateSize) -> str:
+    """Return alphanumeric row:
+    For well plate size < 96, coordinates range from A1 to H12
+    For well plate size >= 96, coordinates range from A01 to P24
+    For well plate size 1536, coordinates range from A01 to AF48
     """
-    # The y-position should be converted to an uppercase well letter
-    row = ""
-    if y >= 26:
-        row = "A"
-    row = row + string.ascii_uppercase[y % 26]
+    num_row = PLATE_DIMS[size][0]
+    num_col = PLATE_DIMS[size][1]
+    if row >= num_row or col >= num_col:
+        msg = f"({row},{col}) out of range for plate size {size.value}[{num_row},{num_col}]"
+        raise ValueError(msg)
 
-    return f"{row}{x + 1:02d}" if size.value >= 96 else f"{row}{x + 1}"
+    row_alpha = ""
+    if row > 26:
+        row_alpha = "A"
+        row -= 26
+    row_alpha = row_alpha + string.ascii_uppercase[row % 26]
 
-
-def build_df(
-    file_paths: list[tuple[pathlib.Path, pathlib.Path]],
-) -> pandas.DataFrame:
-    """Build a DataFrame with well intensities.
-
-    Args:
-        file_paths: List of tuples with image and mask paths.
-
-    Returns:
-        Pandas DataFrame with well intensities.
-    """
-    intensities: list[tuple[float, list[int]]] = []
-
-    if not ADD_TEMP:
-        raise NotImplementedError
-
-    if len(file_paths) < 1:
-        raise ValueError(
-            "provide at least 2 images on the temperature interval"
-            + f"{TEMPERATURE_RANGE[0]}-{TEMPERATURE_RANGE[1]}",
-        )
-
-    for i, (image_path, mask_path) in enumerate(file_paths):
-        temp = TEMPERATURE_RANGE[0] + i / (len(file_paths) - 1) * (
-            TEMPERATURE_RANGE[1] - TEMPERATURE_RANGE[0]
-        )
-        row = (temp, extract_intensities(image_path, mask_path))
-        intensities.append(row)
-
-    # sort intensities by temperature
-    intensities.sort(key=lambda x: x[0])
-
-    # check the first plate for number of wells
-    nb_wells = len(intensities[0][1])
-    plate_size = PlateSize(nb_wells)
-
-    # build header
-    header = ["Temperature"]
-    plate_row = range(PLATE_DIMS[plate_size][0])
-    plate_col = range(PLATE_DIMS[plate_size][1])
-
-    for y, x in itertools.product(plate_row, plate_col):
-        header.append(index_to_battleship(x, y, plate_size))
-
-    # build DataFrame
-    rows = [[round(measure[0], 1), *measure[1]] for measure in intensities]
-    df = pandas.DataFrame(rows, columns=header)
-
-    # Set the temperature as the index
-    df.set_index("Temperature", inplace=True)
-
-    # Sort the roxws by temperature
-    df.sort_index(inplace=True)
-
-    return df
+    return f"{row_alpha}{col+1:02d}" if size.value >= 96 else f"{row_alpha}{col+1}"
