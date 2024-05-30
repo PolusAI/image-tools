41ffcf21fb0d3f90320e01dc76b43f8ea0575cd6
Najib Ishaq <najib_ishaq@zoho.com>
2024-05-30 15:33:55 -0400

    wip: MRE for Samee

---
+++ segmentation/imagej-threshold-apply-tool/run-plugin.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+version=$(<VERSION)
+datapath=$(readlink --canonicalize ../../data)
+
+# Inputs
+opName=ApplyManualThreshold
+inpDir=/data/input
+threshold=30000
+outDir=/data/output
+
+# Output paths
+out=/data/output
+
+docker run --mount type=bind,source=${datapath},target=/data/ \
+            polusai/imagej-threshold-apply-plugin:${version} \
+            --opName ${opName} \
+            --inpDir ${inpDir} \
+            --threshold ${threshold} \
+            --outDir ${outDir}
