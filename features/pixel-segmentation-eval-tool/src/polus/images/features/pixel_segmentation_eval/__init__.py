31746f522d2f82eb435ef792cca347be4f6763ae
Najib <32004868+nishaq503@users.noreply.github.com>
2024-07-19 16:27:08 -0400

    Pinning pyarrow under 17.0 for tools using vaex (#553)
    * build: pinning pyarrow under 17.0
    
    * fix: numba and llvmlite versions in region-segmentation-eval-tool

--- features/pixel-segmentation-eval-tool/src/polus/images/features/pixel_segmentation_eval/__init__.py
+++ features/pixel-segmentation-eval-tool/src/polus/images/features/pixel_segmentation_eval/__init__.py
@@ -1,3 +1,3 @@
 """Pixel segmentation eval package."""
 
-__version__ = "0.1.12"
+__version__ = "0.1.13-dev0"
--- features/region-segmentation-eval-tool/src/polus/images/features/region_segmentation_eval/__init__.py
+++ features/region-segmentation-eval-tool/src/polus/images/features/region_segmentation_eval/__init__.py
@@ -1,2 +1,2 @@
 """Region segmentation eval package."""
-__version__ = "0.2.5"
+__version__ = "0.2.6-dev0"
