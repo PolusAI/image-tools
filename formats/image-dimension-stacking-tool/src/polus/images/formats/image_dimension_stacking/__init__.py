6d42b89d78f12444e0631366ed315553f38c8b9c
Najib Ishaq <najib_ishaq@zoho.com>
2024-09-10 12:28:28 -0400

    Bump version: 0.1.2 → 0.2.0-dev0

--- formats/image-dimension-stacking-tool/src/polus/images/formats/image_dimension_stacking/__init__.py
+++ formats/image-dimension-stacking-tool/src/polus/images/formats/image_dimension_stacking/__init__.py
@@ -4,7 +4,7 @@ from . import utils
 from .dimension_stacking import copy_stack
 from .dimension_stacking import write_stack
 
-__version__ = "0.1.2"
+__version__ = "0.2.0-dev0"
 
 __all__ = [
     "utils",
