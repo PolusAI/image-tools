8ecb08afe42859607a8718aefe07212fda0967a0
Ji Liu <liuji1031@gmail.com>
2025-01-27 09:48:12 -0500

    Updated tool name and path

--- formats/pyramid-generator-3d-tool/build-docker.sh
+++ formats/pyramid-generator-3d-tool/build-docker.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 
 # Change the name of the tool here
-tool_dir="formats/pyramid-generator-3d"
+tool_dir="formats/"
+tool_name="pyramid-generator-3d-tool"
 
 # The version is read from the VERSION file
 version=$(<VERSION)
-tag="polusai/pyramid-generator-3d-tool:${version}"
+tag="polusai/${tool_name}:${version}"
 echo "Building docker image with tag: ${tag}"
 
 # The current directory and the repository root are saved in variables
 cur_dir=$(pwd)
 repo_root=$(git rev-parse --show-toplevel)
+echo "Repository root: ${repo_root}"
 
 # The Dockerfile and .dockerignore files are copied to the repository root before building the image
 cd ${repo_root}
-cp ./${tool_dir}/Dockerfile .
+cp ./${tool_dir}/${tool_name}/Dockerfile .
 cp .gitignore .dockerignore
 docker build . -t ${tag}
 rm Dockerfile .dockerignore
