0a18d39558caa09f4896dd8549724e5ace8b8c48
Hamdah Shafqat Abbasi <hamdahshafqat@hotmail.com>
2025-04-02 14:46:22 -0400

    fix the bug for flex file

--- formats/ome-converter-tool/run-plugin.sh
+++ formats/ome-converter-tool/run-plugin.sh
@@ -15,7 +15,7 @@ outDir=/data/output
 # docker run polusai/ome-converter-plugin:${version}
 
 # Run the plugin
-docker run -e POLUS_IMG_EXT=${fileExtension} --mount type=bind,source=${datapath},target=/data/ \
+docker run -e POLUS_IMG_EXT=${fileExtension} -e NUM_THREADS=4 --mount type=bind,source=${datapath},target=/data/ \
             polusai/ome-converter-tool:${version} \
             --inpDir ${inpDir} \
             --filePattern ${filePattern} \
