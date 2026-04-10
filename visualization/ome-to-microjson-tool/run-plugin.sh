9371e65198327114257a92d8be2ef114478a2ac4
Hamdah Shafqat Abbasi <74803092+hamshkhawar@users.noreply.github.com>
2026-04-10 09:03:38 -0400

    Ome microjson nyxus (#578)
    * updated examples in microjson
    
    * integrated nyxus features
    
    * updating base container image

--- visualization/ome-to-microjson-tool/run-plugin.sh
+++ visualization/ome-to-microjson-tool/run-plugin.sh
@@ -5,17 +5,24 @@ datapath=$(readlink --canonicalize data)
 echo ${datapath}
 
 # Inputs
-inp_dir=${datapath}/input
-file_pattern=".*.ome.tif"
+int_dir=${datapath}/input
+seg_dir=${datapath}/segmentations
+file_pattern="x{x:d+}_y{y:d+}_p{p:d+}_c{c:d+}.ome.tif"
 polygon_type='encoding'
+features="MEAN,MEDIAN"
+neighbor_dist=5
+pixel_per_micron=1.0
 out_dir=${datapath}/output
 
-# #Show the help options
-# #docker run polusai/ome-to-microjson-plugin:${version}
 
 docker run -v ${datapath}:${datapath} \
-            polusai/ome-to-microjson-plugin:${version} \
-            --inpDir ${inp_dir} \
+            polusai/ome-to-microjson-tool:${version} \
+            --intDir ${inp_dir} \
+            --segDir ${seg_dir} \
             --filePattern ${file_pattern} \
             --polygonType ${polygon_type} \
-            --outDir ${out_dir}
+            --features ${features} \
+            --neighborDist ${neighbor_dist} \
+            --pixelPerMicron ${pixel_per_micron} \
+            --outDir ${out_dir} \
+            --tileJson
